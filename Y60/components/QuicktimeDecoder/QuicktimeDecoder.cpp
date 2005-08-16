ba77902b05cc7bb6520375e1f47d9e18d291978c
valentin <valentin@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-08-16 10:52:04 +0000

    Added a simple quicktime decoder for playing mov using quicktime lib and not ffmpeg, which has problems with some movs. Only video frames are supported.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@1654 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ Y60/components/QuicktimeDecoder/QuicktimeDecoder.cpp
@@ -0,0 +1,221 @@
+//=============================================================================
+// Copyright (C) 2004,2005, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//=============================================================================
+//
+//    $RCSfile: QuicktimeDecoder.cpp,v $
+//     $Author: ulrich $
+//   $Revision: 1.6 $
+//       $Date: 2005/04/01 17:03:27 $
+//
+//  Quicktime movie decoder.
+//
+//=============================================================================
+
+#include "QuicktimeDecoder.h"
+
+#include <qt/QTML.h>
+#include <qt/TextUtils.h>
+
+#include <asl/Logger.h>
+#include <asl/file_functions.h>
+
+#define DB(x) //x
+
+using namespace std;
+
+extern "C"
+EXPORT asl::PlugInBase * y60QuicktimeDecoder_instantiatePlugIn(asl::DLHandle myDLHandle) {
+    return new y60::QuicktimeDecoder(myDLHandle);
+}
+Movie QTURL_NewMovieFromURL (char *theURL);
+
+namespace y60 {
+
+    // QuicktimeDecoder
+    QuicktimeDecoder::QuicktimeDecoder(asl::DLHandle theDLHandle) :
+        PlugInBase(theDLHandle), _myLastDecodedFrame(UINT_MAX),_myInternalMovieTime(0)
+    {}
+
+    QuicktimeDecoder::~QuicktimeDecoder() {
+        closeMovie();
+    }
+
+    asl::Ptr<MovieDecoderBase> QuicktimeDecoder::instance() const {
+        InitializeQTML(0);        
+        EnterMovies();        
+        return asl::Ptr<MovieDecoderBase>(new QuicktimeDecoder(getDLHandle()));
+    }
+
+    std::string
+    QuicktimeDecoder::canDecode(const std::string & theUrl, asl::ReadableStream * theStream) {
+        if (asl::toLowerCase(asl::getExtension(theUrl)) == "mov") {
+            AC_INFO << "QuicktimeDecoder1 can decode :" << theUrl << endl;
+            return MIME_TYPE_MOV;
+        } else {
+            AC_INFO << "QuicktimeDecoder1 can not decode :" << theUrl << " responsible for extension mov" << endl;
+            return "";
+        }
+    }
+
+    void
+    QuicktimeDecoder::closeMovie() {
+        AC_DEBUG << "QuicktimeDecoder::closeMovie";
+    }
+
+    void
+    QuicktimeDecoder::stopMovie() {
+        _myLastDecodedFrame = UINT_MAX;
+        _myInternalMovieTime = 0;
+
+        MovieDecoderBase::stopMovie();
+    }
+
+    bool
+    QuicktimeDecoder::hasVideo() const {
+        return true;
+    }
+
+    bool
+    QuicktimeDecoder::hasAudio() const {
+        return false;
+    }
+
+    void
+    QuicktimeDecoder::load(const std::string & theFilename) {
+        asl::Time myLoadStartTime;
+        
+        OSErr                   myErr;
+        short                   myFileRefNum;            
+        char                    myFullPath[255];        
+        
+        //_myMovie = QTURL_NewMovieFromURL((char*)theFilename.c_str()); 
+        
+        // qt can not handle filepaths like this: "./foo.mov", 
+        // which comes from the PacketManager -> cut it off
+        if (theFilename.size() > 2 && theFilename.substr(0,2) == "./" ) {
+            std::string myNewFilename = theFilename.substr(2,theFilename.size() - 2);
+            strcpy (myFullPath, myNewFilename.c_str());            
+        } else {
+            strcpy (myFullPath, theFilename.c_str());
+        }        
+        c2pstr( (char*)myFullPath );
+        FSSpec sfFile;        
+        FSMakeFSSpec (0, 0L, ConstStr255Param(myFullPath), &sfFile); 
+                   
+        myErr = OpenMovieFile (&sfFile, &myFileRefNum, fsRdPerm);
+        if (myErr != noErr) {
+            throw QuicktimeDecoderException(string("Movie ") + theFilename +
+                " could not be loaded, check existence.", PLUS_FILE_LINE);
+        }
+        
+        myErr = NewMovieFromFile (&_myMovie, myFileRefNum, 0, 0, newMovieActive, 0);    
+        if (myErr != noErr) {
+            throw QuicktimeDecoderException(string("Movie ") + theFilename +
+                " could not be loaded, check existence.", PLUS_FILE_LINE);
+        }
+
+        CloseMovieFile(myFileRefNum);
+                  
+        Rect movieBounds;
+        GetMovieBox(_myMovie, &movieBounds);
+
+        setPixelFormat(PixelEncoding(RGBA));
+        setFrameRate(25);
+        setFrameWidth(movieBounds.right);
+        setFrameHeight(movieBounds.bottom);
+        double myDurationInSeconds = float(GetMovieDuration(_myMovie)) / GetMovieTimeScale(_myMovie);
+        setFrameCount(long(myDurationInSeconds * 25));
+        
+        QTNewGWorld(&_myOffScreenWorld, k32RGBAPixelFormat, &movieBounds, 0, 0, 0);
+        SetGWorld(_myOffScreenWorld, NULL);
+        SetMovieGWorld(_myMovie, _myOffScreenWorld, NULL);
+        
+        asl::Time myLoadEndTime;
+        AC_INFO << "Load file:" << theFilename << ", time " << (myLoadEndTime - myLoadStartTime) << "s";
+    }
+
+    double
+    QuicktimeDecoder::readFrame(double theTime, unsigned theFrame, dom::ResizeableRasterPtr theTargetRaster) {
+        AC_INFO << "Read frame: " << theFrame << ", last decoded frame: " << _myLastDecodedFrame;
+
+        if (theFrame >= getFrameCount() -1) {
+            setEOF(true);
+            return theTime;
+        }
+        // Handle frame wrap around
+        if (_myLastDecodedFrame > theFrame) {
+            decodeFrame(0, theTargetRaster);
+        }
+
+        if (theFrame != _myLastDecodedFrame) {
+            decodeFrame(theFrame, theTargetRaster);
+        }
+        
+        theTargetRaster->resize(getFrameWidth(), getFrameHeight());
+        return theTime;
+    }
+
+    void QuicktimeDecoder::decodeFrame(unsigned theFrameNumber, dom::ResizeableRasterPtr theTargetRaster)
+    {    
+        TimeValue myDuration;
+        
+        SetGWorld(_myOffScreenWorld, NULL);
+        
+        // get the next frame of the source movie
+        short   myFlags     = nextTimeMediaSample;
+        OSType  myMediaType = FOUR_CHAR_CODE('vide');//VIDEO_TYPE;
+        
+        // if this is the first frame, include the frame we are currently on
+        if (theFrameNumber == 0) {
+            myFlags |= nextTimeEdgeOK;
+        }
+
+        // skip to the next interesting time and get the duration for that frame
+        GetMovieNextInterestingTime(_myMovie,
+                  myFlags,
+                  1,
+                  &myMediaType,
+                  _myInternalMovieTime,
+                  0,
+                  &_myInternalMovieTime,
+                  &myDuration);
+        // set the time for the frame and give time to the movie toolbox
+        SetMovieTimeValue(_myMovie, _myInternalMovieTime);
+        
+        // *** this does the actual drawing into the GWorld ***
+        MoviesTask(_myMovie,0);
+        Ptr baseAddr = GetPixBaseAddr(GetGWorldPixMap(_myOffScreenWorld)); 
+        QTGetPixMapHandleRowBytes(GetGWorldPixMap(_myOffScreenWorld));    
+        memcpy(theTargetRaster->pixels().begin(), baseAddr, theTargetRaster->pixels().size());
+             
+        _myLastDecodedFrame = theFrameNumber;
+
+    }    
+}
+
+Movie QTURL_NewMovieFromURL (char *theURL)
+{
+  Movie    myMovie = NULL;
+  Handle    myHandle = NULL;
+  Size    mySize = 0;
+
+  mySize = (Size)strlen(theURL) + 1;
+  cout << " mySize: " << mySize << endl;
+  myHandle = NewHandleClear(mySize);
+  BlockMove(theURL, *myHandle, mySize);
+  
+  NewMovieFromDataRef(&myMovie, newMovieActive, 0, myHandle, URLDataHandlerSubType);
+
+  if (myHandle != NULL) {
+    DisposeHandle(myHandle);
+  }
+
+  return myMovie;
+
+}   
---
+++ Y60/components/QuicktimeDecoder/QuicktimeDecoder.h
@@ -0,0 +1,101 @@
+//=============================================================================
+// Copyright (C) 2003, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//=============================================================================
+//
+//   $RCSfile: QuicktimeDecoder.h,v $
+//   $Author: valentin $
+//   $Revision: 1.4 $
+//   $Date: 2005/04/01 10:12:21 $
+//
+//  Quicktime movie decoder.
+//
+//=============================================================================
+
+#ifndef _ac_y60_QuicktimeDecoder_h_
+#define _ac_y60_QuicktimeDecoder_h_
+
+#include <asl/PlugInBase.h>
+#include <y60/MovieDecoderBase.h>
+
+#include <string>
+#include <vector>
+
+#ifdef WIN32
+// Disable Quicktime Warning in Windows
+#pragma warning(push)
+#pragma warning(disable:4244)
+#define EMULATE_INTTYPES
+#endif
+
+#include <qt/Movies.h>
+
+#ifdef WIN32
+// Reenable Warning in Windows
+#pragma warning(pop)
+#endif
+
+namespace y60 {
+
+    const std::string MIME_TYPE_MOV = "application/mov";
+
+    DEFINE_EXCEPTION(QuicktimeDecoderException, asl::Exception);
+
+    class QuicktimeDecoder : 
+        public MovieDecoderBase,
+        public asl::PlugInBase
+    {
+        public:
+            QuicktimeDecoder(asl::DLHandle theDLHandle);
+            virtual ~QuicktimeDecoder();
+    
+            bool hasVideo() const;
+            bool hasAudio() const;
+            void stopMovie();
+
+            /**
+             * loads a movie from the file given by theFilename
+             * @param theFilename file to load into the decoder
+             */
+            void load(const std::string & theFilename);
+    
+            /**
+             * loads a movie from the stream given by theSource
+             * @param theFilename file to identify theSource
+             */
+            void load(asl::Ptr<asl::ReadableStream> theSource, const std::string & theFilename) {
+                throw NotYetImplemented(JUST_FILE_LINE);
+            }
+        
+            virtual asl::Ptr<MovieDecoderBase> instance() const;
+            std::string canDecode(const std::string & theUrl, asl::ReadableStream * theStream = 0);
+    
+            /**
+             * Reads a frame.
+             * @param theTime time of the frame to deliver
+             * @param theFrame is ignored by this decoder
+             * @param theTargetRaster image raster to render the frame into
+             * @return timestamp of the frame delivered in theTargetRaster
+             */
+            double readFrame(double theTime, unsigned theFrame, dom::ResizeableRasterPtr theTargetRaster);
+    
+        private:
+            void closeMovie();
+            void decodeFrame(unsigned theFrameNumber, dom::ResizeableRasterPtr theTargetRaster);
+
+            ::Movie                  _myMovie;    
+            unsigned                 _myLastDecodedFrame;
+            ::TimeValue              _myInternalMovieTime;  
+            GWorldPtr                _myOffScreenWorld;
+
+        };
+    
+        typedef asl::Ptr<QuicktimeDecoder> QuicktimeDecoderPtr;
+}
+
+#endif
