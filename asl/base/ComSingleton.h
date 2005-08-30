9a9109f95373947ca921978a4ca35c197f21b31b
christian <christian@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-08-30 11:29:04 +0000

    added Com singleton
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@2599 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ asl/base/ComSingleton.h
@@ -0,0 +1,59 @@
+//============================================================================
+//
+// Copyright (C) 2005, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//============================================================================
+//
+//   $RCSfile: ComSingleton.h,v $
+//
+//   $Author: thomas $
+//
+//   $Revision: 1.1 $
+//
+//=============================================================================
+
+#ifndef _ac_ComSingleton_h_
+#define _ac_ComSingleton_h_
+
+#include <asl/Singleton.h>
+
+namespace asl {
+
+    /**
+    * ComSingleton is a singleton that is used by the WMVDecoder to check
+    * if COM is already initialized. 
+    */
+    class ComSingleton : public Singleton<ComSingleton>
+    { 
+        friend class SingletonManager;
+
+    public:
+        ComSingleton() : _myDecoderCount(0) {
+        }
+
+        unsigned getComInitialized() const {
+            return _myDecoderCount;
+        }
+
+        /**
+        * Increments COM ref count and initialize for first user.
+        */
+        void ref();
+
+        /**
+        * Decrements COM reference count and uninitialize for last user.
+        */
+        void unref();
+
+    private:
+        unsigned _myDecoderCount;  ///< Internal counter
+    };
+
+}
+
+#endif // _ac_ComSingleton_h_
