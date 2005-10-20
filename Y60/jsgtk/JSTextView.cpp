7d77d69d6d1adef6365b97a024f5d3521362ea23
martin <martin@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-10-20 08:06:11 +0000

    added missing file
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@3167 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ Y60/jsgtk/JSTextView.cpp
@@ -0,0 +1,162 @@
+//=============================================================================
+// Copyright (C) 2003, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//=============================================================================
+//
+//   $RCSfile: JSTextView.cpp,v $
+//   $Author: martin $
+//   $Revision: 1.3 $
+//   $Date: 2005/04/21 16:25:02 $
+//
+//
+//=============================================================================
+
+#include "JSTextView.h"
+#include "jsgtk.h"
+#include <y60/JScppUtils.h>
+#include <iostream>
+
+using namespace std;
+using namespace asl;
+
+namespace jslib {
+
+
+static JSBool
+toString(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    DOC_BEGIN("");
+    DOC_END;
+    std::string myStringRep = string("Gtk::TextView@") + as_string(obj);
+    JSString * myString = JS_NewStringCopyN(cx,myStringRep.c_str(),myStringRep.size());
+    *rval = STRING_TO_JSVAL(myString);
+    return JS_TRUE;
+}
+
+JSFunctionSpec *
+JSTextView::Functions() {
+    IF_REG(cerr << "Registering class '"<<ClassName()<<"'"<<endl);
+    static JSFunctionSpec myFunctions[] = {
+        // name                  native                   nargs
+        {"toString",             toString,                0},
+        {0}
+    };
+    return myFunctions;
+}
+
+JSPropertySpec *
+JSTextView::Properties() {
+    static JSPropertySpec myProperties[] = {
+        {"buffer", PROP_buffer, JSPROP_ENUMERATE|JSPROP_PERMANENT},
+        {0}
+    };
+    return myProperties;
+}
+
+// getproperty handling
+JSBool
+JSTextView::getPropertySwitch(unsigned long theID, JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
+    JSClassTraits<NATIVE>::ScopedNativeRef myObj(cx, obj);
+    return getPropertySwitch(myObj.getNative(), theID, cx, obj, id, vp);
+}
+
+JSBool
+JSTextView::setPropertySwitch(unsigned long theID, JSContext *cx, JSObject *obj, jsval id, jsval *vp) {
+    JSClassTraits<NATIVE>::ScopedNativeRef myObj(cx, obj);
+    return setPropertySwitch(myObj.getNative(), theID, cx, obj, id, vp);
+}
+
+JSBool
+JSTextView::getPropertySwitch(NATIVE & theNative, unsigned long theID,
+        JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+    switch (theID) {
+        case PROP_buffer:
+            *vp = as_jsval(cx, theNative.get_buffer()->get_text());
+            return JS_TRUE;
+        default:
+            return JSBASE::getPropertySwitch(theNative, theID, cx, obj, id, vp);
+    }
+}
+JSBool
+JSTextView::setPropertySwitch(NATIVE & theNative, unsigned long theID,
+        JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+    switch (theID) {
+        case PROP_buffer:
+            try {
+                Glib::ustring theText;
+                convertFrom(cx, *vp, theText);
+                theNative.get_buffer()->set_text(theText);
+                return JS_TRUE;
+            } HANDLE_CPP_EXCEPTION;
+        default:
+            return JSBASE::setPropertySwitch(theNative, theID, cx, obj, id, vp);
+    }
+}
+
+JSBool
+JSTextView::Constructor(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    DOC_BEGIN("");
+    DOC_END;
+    if (JSA_GetClass(cx,obj) != Class()) {
+        JS_ReportError(cx,"Constructor for %s  bad object; did you forget a 'new'?",ClassName());
+        return JS_FALSE;
+    }
+
+    NATIVE * newNative = 0;
+
+    JSTextView * myNewObject = 0;
+
+    if (argc == 0) {
+        newNative = new Gtk::TextView();
+        myNewObject = new JSTextView(OWNERPTR(newNative), newNative);
+    } else {
+        JS_ReportError(cx,"Constructor for %s: bad number of arguments: expected none () %d",ClassName(), argc);
+        return JS_FALSE;
+    }
+
+    if (myNewObject) {
+        JS_SetPrivate(cx,obj,myNewObject);
+        return JS_TRUE;
+    }
+    JS_ReportError(cx,"JSTextView::Constructor: bad parameters");
+    return JS_FALSE;
+}
+
+void
+JSTextView::addClassProperties(JSContext * cx, JSObject * theClassProto) {
+    JSBASE::addClassProperties(cx, theClassProto);
+    JSA_AddFunctions(cx, theClassProto, Functions());
+    JSA_AddProperties(cx, theClassProto, Properties());
+    createClassModuleDocumentation("gtk", ClassName(), Properties(), Functions(), 0, 0, 0,
+                JSBASE::ClassName());
+}
+
+JSObject *
+JSTextView::initClass(JSContext *cx, JSObject *theGlobalObject) {
+    JSObject * myClassObject = Base::initClass(cx, theGlobalObject, ClassName(), Constructor, 0,0);
+    if (myClassObject) {
+        addClassProperties(cx, myClassObject);
+    }
+    jsval myConstructorFuncObjVal;
+    if (JS_GetProperty(cx, theGlobalObject, ClassName(), &myConstructorFuncObjVal)) {
+//        JSObject * myConstructorFuncObj = JSVAL_TO_OBJECT(myConstructorFuncObjVal);
+//        JSA_DefineConstInts(cx, myConstructorFuncObj, ConstIntProperties());
+    } else {
+        cerr << "JSTextView::initClass: constructor function object not found, could not initialize static members"<<endl;
+    }
+    return myClassObject;
+}
+
+jsval as_jsval(JSContext *cx, JSTextView::OWNERPTR theOwner, JSTextView::NATIVE * theNative) {
+    JSObject * myReturnObject = JSTextView::Construct(cx, theOwner, theNative);
+    return OBJECT_TO_JSVAL(myReturnObject);
+}
+
+}
+
