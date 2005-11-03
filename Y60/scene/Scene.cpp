45d6f417272502fd74afed1fa8ec4c3aa22e2e3a
ulrich <ulrich@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-11-03 17:36:30 +0000

    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@3427 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/scene/Scene.cpp
+++ Y60/scene/Scene.cpp
@@ -1,20 +1,11 @@
 //=============================================================================
-// Copyright (C) 2003, ART+COM AG Berlin
+// Copyright (C) 2003-2005, ART+COM AG Berlin
 //
 // These coded instructions, statements, and computer programs contain
 // unpublished proprietary information of ART+COM AG Berlin, and
 // are copy protected by law. They may not be disclosed to third parties
 // or copied or duplicated in any form, in whole or in part, without the
 // specific, prior written permission of ART+COM AG Berlin.
-//=============================================================================
-//
-//   $RCSfile: Scene.cpp,v $
-//   $Author: martin $
-//   $Revision: 1.178 $
-//   $Date: 2005/04/29 14:09:10 $
-//
-//  Description: A simple scene class.
-//
 //=============================================================================
 
 #include "Scene.h"
