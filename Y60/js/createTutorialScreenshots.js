94fd39eb9a0b4354f7da4137192b4c269ac1d787
christian <christian@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-11-14 17:23:40 +0000

    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@3673 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/js/createTutorialScreenshots.js
+++ Y60/js/createTutorialScreenshots.js
@@ -1,37 +1,53 @@
+//=============================================================================
+// Copyright (C) 2003, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//=============================================================================
+//
+//   $RCSfile: scenetest.js,v $
+//   $Author: martin $
+//   $Revision: 1.1 $
+//   $Date: 2004/08/03 17:17:03 $
+//
+//
+//=============================================================================
 
-const myFrameBreak = 50;
-const myScreenshotSize = [200,200];
+const ourFrameBreak      = 5;
+const ourScreenshotSize  = [200,200];
+const ourTutorialPath    = arguments[0];
+const ourTargetDirectory = arguments[1];
 
-var myFrameCount = 0;
-var myTutorialPath = arguments[0];
-var myTargetDirectory = arguments[1];
+var ourFrameCount        = 0;
 
-if (!fileExists(myTutorialPath) || !isDirectory(myTargetDirectory)) {
+if (!fileExists(ourTutorialPath) || !isDirectory(ourTargetDirectory)) {
     print("Usage: createTutorialScreenshots.js TUTORIAL_SCRIPT TARGET_DIRECTORY");
     exit(1);
 }
 
-
 RenderWindow.prototype.onProtoFrame = function(theTime) {
     this.fixedFrameTime = 0.04;
-    if (myFrameCount == myFrameBreak - 5) {
-        this.resize(myScreenshotSize[0], myScreenshotSize[1]);
-    } else if (myFrameCount == myFrameBreak) {
-        var myTutorial = basename(myTutorialPath);
+    if (ourFrameCount == 0) {
+        this.resize(ourScreenshotSize[0], ourScreenshotSize[1]);
+    } else if (ourFrameCount == ourFrameBreak) {
+        var myTutorial = basename(ourTutorialPath);
         var myDotIndex = myTutorial.lastIndexOf(".");
         if (myDotIndex != -1 && myTutorial.substring(myDotIndex, myTutorial.length) == ".js") {
             var myScreenshotFileName = myTutorial.substr(0, myDotIndex) + ".png";
-            print ("Generating screenshot " + myTargetDirectory + "/" + myScreenshotFileName);
-            this.saveBuffer(myTargetDirectory + "/" + myScreenshotFileName);
+            print ("Generating screenshot " + ourTargetDirectory + "/" + myScreenshotFileName);
+            this.saveBuffer(ourTargetDirectory + "/" + myScreenshotFileName);
             exit(0);
         } else {
             print("### ERROR: Not a valid js file: " + myTutorial);
             exit(1);
         }
     }
-    myFrameCount++;
+    ourFrameCount++;
 }
 
 includePath("${PRO}/tutorials");
 
-use(myTutorialPath);
+use(ourTutorialPath);
