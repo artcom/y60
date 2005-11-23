cf008c22b23017f176b399ee3e82a87ce3d2bb31
jens <jens@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-11-23 11:21:06 +0000

    added product code
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@3875 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/products/watchdog/app/Makedist.pm
+++ Y60/products/watchdog/app/Makedist.pm
@@ -2,6 +2,7 @@ package Makedist;
 use MakedistTools;
 
 $PRODUCT_NAME   = "watchdog";
+$PRODUCT_CODE   = "1435b359-ec2f-40ac-a604-80fe134e52bf";
 
 sub runLocalMakedist($) {    
     my $theDistDir = $_[0];
