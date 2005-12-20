d5f688c2284d5a7a32f37ce6dcd1d1787ff0e803
christian <christian@f3027e6b-e3f5-0310-a8e1-edde99107618>
2005-12-20 18:46:51 +0000

    added jsdoc example
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@4260 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/js/BSpline.js
+++ Y60/js/BSpline.js
@@ -7,54 +7,52 @@
 // or copied or duplicated in any form, in whole or in part, without the
 // specific, prior written permission of ART+COM AG Berlin.
 //=============================================================================
-//
-//   $RCSfile: BSpline.js,v $
-//   $Author: valentin $
-//   $Revision: 1.13 $
-//   $Date: 2004/08/06 15:38:57 $
-//
-//
-//=============================================================================
 
-// Constructor
-function BSpline() {    
-    this.Constructor(this);
-}
+/**
+ * @fileoverview JavaScript BSpline class with ease in and out
+ * @author Christian Hardenberg
+ */
 
-BSpline.prototype.Constructor = function(obj) {
-    var _myResult = new Array();
-
-    obj.getResult = function() {
+/**
+ * Constructs a new BSpline object
+ */
+function BSpline() {
+    /**
+     * Gets the point array of the BSpline
+     * @return {ArrayOfPoint3f} The point array
+     */
+    this.getResult = function() {
         return _myResult;
     }
 
-    /*
-        theStart       --- Startpunkt (new Vector3f)
-        theStartHandle --- Anfasser-Startpunkt (new Vector3f)
-        theEnd         --- Endpunkt (new Vector3f)
-        theEndHandle   --- Anfasser-Endpunkt (new Vector3f)
-        theResolution  --- Anzahl der schritte, von anfang bis ende
-        theEaseIn      --- Wert zwischen 0 und 1. 1 für maximales theEaseIn
-        theEaseOut     --- Wert zwischen 0 und 1. 1 für maximales theEaseOut
-    */    
-    obj.calculate = function(theStart, theStartHandle, theEnd, theEndHandle, 
-                             theResolution, theEaseIn, theEaseOut) 
+    /**
+     *   Calculate the BSpline point array
+     *   @param {Vector3f} theStart       Startpunkt
+     *   @param {Vector3f} theStartHandle Anfasser-Startpunkt
+     *   @param {Vector3f} theEnd         Endpunkt
+     *   @param {Vector3f} theEndHandle   Anfasser-Endpunkt
+     *   @param {integer}  theResolution  Anzahl der schritte, von anfang bis ende
+     *   @param {float}    theEaseIn      Wert zwischen 0 und 1. 1 für maximales theEaseIn
+     *   @param {float}    theEaseOut     Wert zwischen 0 und 1. 1 für maximales theEaseOut
+     */
+    this.calculate = function(theStart, theStartHandle, theEnd, theEndHandle,
+                             theResolution, theEaseIn, theEaseOut)
     {
         // Clear last result
         _myResult = [];
-        
+
         if (theEaseIn == null) {
             theEaseIn = 0;
         }
         if (theEaseOut == null) {
             theEaseOut = 0;
         }
-        
+
         theEaseOut = 1 - theEaseOut;
-        
+
         // anzahl der Dimensionen
         var myDimensionCount = 3;
-        
+
         // punkte umsortieren
         var myP0 = new Array(); // punkt 1
         var myP1 = new Array(); // anfasser 1
@@ -66,44 +64,42 @@ BSpline.prototype.Constructor = function(obj) {
             myP3[j] = theEnd[j];
             myP2[j] = theEndHandle[j];
         }
-        
+
         // b-Funktion
         var a = new Array();
         var b = new Array();
         var c = new Array();
-        
+
         for (var j = 0; j < myDimensionCount; j++) {
             c[j] = 3 * (myP1[j] - myP0[j]);
             b[j] = 3 * (myP2[j] - myP1[j]) - c[j];
             a[j] = myP3[j] - myP0[j] - c[j] - b[j];
         }
-        
-        // fill ease array 
+
+        // fill ease array
         var myEaseArray = calcEaseModifier(theEaseIn, theEaseOut, theResolution);
         for (var i = 0; i < theResolution; ++i) {
             // ease
             var t = myEaseArray[i];
-            //print("t: " + t);            
+            //print("t: " + t);
             // b-function
-            var x = a[0] * t * t * t + 
-                    b[0] * t * t + 
+            var x = a[0] * t * t * t +
+                    b[0] * t * t +
                     c[0] * t +
                     myP0[0];
-            var y = a[1] * t * t * t + 
-                    b[1] * t * t + 
+            var y = a[1] * t * t * t +
+                    b[1] * t * t +
                     c[1] * t +
                     myP0[1];
-            var z = a[2] * t * t * t + 
-                    b[2] * t * t + 
+            var z = a[2] * t * t * t +
+                    b[2] * t * t +
                     c[2] * t +
                     myP0[2];
 
-            
-            var myPunkte = new Vector3f(x, y, z);
-            _myResult[i] = myPunkte;
-        }     
+            _myResult[i] = new Vector3f(x, y, z);
+        }
     }
-            
+
     function calcEaseModifier(theEaseIn, theEaseOut, theResolution) {
         var myEaseArray = new Array();
         for (var i = 0; i < theResolution; ++i) {
@@ -112,7 +108,7 @@ BSpline.prototype.Constructor = function(obj) {
         }
         return myEaseArray;
     }
-    
+
     function smoothstep(theEaseIn, theEaseOut, theInput) {
         if (theInput < theEaseIn) {
             return 0.0;
@@ -122,5 +118,10 @@ BSpline.prototype.Constructor = function(obj) {
         }
         var myOutput = (theInput - theEaseIn) / (theEaseOut - theEaseIn);
         return (myOutput * myOutput) * (3 - 2 * myOutput);
-    }        
+    }
+
+    var _myResult = new Array();
 }
+
+// Neccessary for auto-documentation
+new BSpline();
