ca161775f6e478928bc389bb3a17cceb5f1c2917
ulrich <ulrich@f3027e6b-e3f5-0310-a8e1-edde99107618>
2006-02-08 15:51:17 +0000

    brought Valentins changes from Nov/Dec 2005 back from the Dead
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@5074 f3027e6b-e3f5-0310-a8e1-edde99107618

--- netsrc/paintlib/src/Filter/plfilterrotate.cpp
+++ netsrc/paintlib/src/Filter/plfilterrotate.cpp
@@ -13,12 +13,64 @@
 #include "plbitmap.h"
 #include "plfilterrotate.h"
 #include "plpoint.h"
+#include "plexcept.h"
 
+
+//////////////////////////////////////////////////////////////////////
+// PixelType based Template rotation beef
+//////////////////////////////////////////////////////////////////////
+template<class PIXELTYPE>
+void getPixel(PLBmpBase * pBmpSource, int theX, int theY, PIXELTYPE& theResult);
+template<>
+void getPixel(PLBmpBase * pBmpSource, int theX, int theY, PLPixel32& theResult) { theResult = pBmpSource->GetPixel32(theX,theY);}
+template<>
+void getPixel(PLBmpBase * pBmpSource, int theX, int theY, PLPixel24& theResult) { theResult = pBmpSource->GetPixel24(theX,theY);}
+template<>
+void getPixel(PLBmpBase * pBmpSource, int theX, int theY, PLPixel16& theResult) { theResult = pBmpSource->GetPixel16(theX,theY);}
+
+template <class PIXELTYPE>
+void
+rotate(PLBmpBase * pBmpSource, PLBmp * pBmpDest, const PLFilterRotate::AngleType & theAngleType) {
+  int oldWidth = pBmpSource->GetWidth();
+  int oldHeight= pBmpSource->GetHeight();
+  int newWidth = theAngleType == PLFilterRotate::oneeighty ? oldWidth : oldHeight;
+  int newHeight = theAngleType == PLFilterRotate::oneeighty ? oldHeight : oldWidth;
+
+  // Calculate the size of the new bitmap
+  pBmpDest->Create (newWidth, newHeight, pBmpSource->GetPixelFormat(), 
+                    NULL, 0, pBmpSource->GetResolution());
+  PIXELTYPE pix;
+  for (int newY = 0; newY < newHeight; ++newY)
+  {
+    for (int newX = 0; newX < newWidth; ++newX)
+    {
+      int oldX;
+      int oldY;
+      switch(theAngleType)
+      {
+        case PLFilterRotate::ninety:
+          oldX = newY;
+          oldY = oldHeight - newX - 1;
+          break;
+        case PLFilterRotate::oneeighty:
+          oldX = oldWidth - newX - 1;
+          oldY = oldHeight - newY - 1;
+          break;
+        case PLFilterRotate::twoseventy:
+          oldY = newX;
+          oldX = oldWidth - newY - 1;
+          break;
+      }
+      getPixel<PIXELTYPE>(pBmpSource, oldX,oldY, pix);
+      pBmpDest->SetPixel(newX,newY,pix);
+    }
+  }
+    
+}
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
-
 PLFilterRotate::PLFilterRotate(AngleType a)
     : PLFilter(),
       m_Angle(a)
@@ -31,43 +83,23 @@ PLFilterRotate::~PLFilterRotate()
 
 void PLFilterRotate::Apply(PLBmpBase * pBmpSource, PLBmp * pBmpDest) const
 {
-  // Only works for 32 bpp.
-  PLASSERT (pBmpSource->GetBitsPerPixel() == 32);
-
-  int oldWidth = pBmpSource->GetWidth();
-  int oldHeight= pBmpSource->GetHeight();
-  int newWidth = m_Angle == oneeighty ? oldWidth : oldHeight;
-  int newHeight = m_Angle == oneeighty ? oldHeight : oldWidth;
-
-  // Calculate the size of the new bitmap
-  pBmpDest->Create (newWidth, newHeight, pBmpSource->GetPixelFormat(), 
-                    NULL, 0, pBmpSource->GetResolution());
-  PLPixel32 pix;
-  for (int newY = 0; newY < newHeight; ++newY)
-  {
-    for (int newX = 0; newX < newWidth; ++newX)
-    {
-      int oldX;
-      int oldY;
-      switch(m_Angle)
-      {
-        case ninety:
-          oldX = newY;
-          oldY = oldHeight - newX - 1;
-          break;
-        case oneeighty:
-          oldX = oldWidth - newX - 1;
-          oldY = oldHeight - newY - 1;
-          break;
-        case twoseventy:
-          oldY = newX;
-          oldX = oldWidth - newY - 1;
-          break;
-      }
-      pix = pBmpSource->GetPixel32(oldX,oldY);
-      pBmpDest->SetPixel(newX,newY,pix);
-    }
-  }
+    switch (pBmpSource->GetBitsPerPixel()) {
+        case 32:
+            rotate<PLPixel32>(pBmpSource, pBmpDest, m_Angle);
+            break;
+        case 24:
+            rotate<PLPixel24>(pBmpSource, pBmpDest, m_Angle);
+            break;
+        case 16:
+            rotate<PLPixel16>(pBmpSource, pBmpDest, m_Angle);
+            break;
+        case 8:
+            throw (PLTextException (PL_ERRFORMAT_NOT_SUPPORTED, "Unsupported."));
+            //rotate<PLPixel8>(pBmpSource, pBmpDest, m_Angle);
+            break;
+        default:
+            throw (PLTextException (PL_ERRFORMAT_NOT_SUPPORTED, "Unsupported."));
+    };
 }
 
 /*
