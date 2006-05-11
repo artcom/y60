12b26fae0f403e9f07504c161b684787ae0a13a1
jens <jens@f3027e6b-e3f5-0310-a8e1-edde99107618>
2006-05-11 08:14:13 +0000

    local setup windows batch script
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@6312 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ Y60/products/maya-xport/setup.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set MAYA_VERSION=7.0
+set MAYA_PATH=%ProgramFiles%\Alias\Maya%MAYA_VERSION%
+set INSTALL_PATH=%ProgramFiles%\ART+COM\mayaY60export
+set MODULE_FILE=%MAYA_PATH%\modules\mayaY60export.txt
+
+if NOT EXIST %MAYA_PATH% (
+    ECHO Maya not found in default path %MAYA_PATH%
+    EXIT 1
+)
+
+ECHO Creating installation directory %INSTALL_PATH%
+MKDIR %INSTALL_PATH%\plug-ins
+MKDIR %INSTALL_PATH%\scripts
+
+ECHO Installing mayaY60-Exporter files...
+COPY /Y scripts\* "%ProgramFiles%\ART+COM\mayaY60export\scripts"
+COPY /Y plug-ins\* "%ProgramFiles%\ART+COM\mayaY60export\plug-ins"
+COPY /Y *.dll "%MAYA_PATH%\bin"
+
+ECHO Creating module file %MODULE_FILE%
+ECHO "+ mayaY60export %MAYA_VERSION% %INSTALL_PATH%" > %MODULE_FILE%
+
+echo done.
+
+pause
