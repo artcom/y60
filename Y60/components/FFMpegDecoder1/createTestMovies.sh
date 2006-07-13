c2f17533820fdda83b36579ef00cfd86fd5722bd
ralf <ralf@f3027e6b-e3f5-0310-a8e1-edde99107618>
2006-07-13 16:02:16 +0000

    iframe option for testmovie generation
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@7298 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/FFMpegDecoder1/createTestMovies.sh
+++ Y60/components/FFMpegDecoder1/createTestMovies.sh
@@ -11,13 +11,14 @@ MOVIE_HEIGHT=$2
 FRAMERATE=$3
 
 function createImages {
-    y60 CreateTestMovie.js $MOVIE_WIDTH $MOVIE_HEIGHT $FRAMERATE 60
+    y60 CreateTestMovie.js $MOVIE_WIDTH $MOVIE_HEIGHT $FRAMERATE 10
 }
 
 function createMovie {
     # Note that MOVIE_LENGTH_IN_SECONDS must be an integer!
     MOVIE_LENGTH_IN_SECONDS=$1
     USE_AUDIO=$2
+    USE_ONLY_IFRAMES=$3
  
     echo --- Generating ${MOVIE_FILENAME}
 
@@ -31,23 +32,38 @@ function createMovie {
             cp testmovies/OneSecondClick.wav testmovies/tmp/tmp.wav
         fi
         AUDIO_PARAMS="-i testmovies/tmp/tmp.wav"
-        MOVIE_FILENAME=${MOVIE_FILENAME}_audio.mpg
+        MOVIE_FILENAME=${MOVIE_FILENAME}_audio
     else
-        echo "else"
-        AUDIO_PARAMS=
-        MOVIE_FILENAME=${MOVIE_FILENAME}_noaudio.mpg
+        AUDIO_PARAMS=""
+        MOVIE_FILENAME=${MOVIE_FILENAME}_noaudio
     fi
-    ffmpeg ${AUDIO_PARAMS} -t ${MOVIE_LENGTH_IN_SECONDS}.04 -b 500 -i testmovies/tmp/${MOVIE_WIDTH}x${MOVIE_HEIGHT}_frame%07d.png -y -r ${FRAMERATE} -vcodec mpeg2video ${MOVIE_FILENAME} 
-    
+    if [ "$USE_ONLY_IFRAMES" = "true" ]
+    then
+        echo using iframes only
+        IFRAME_PARAMS="-intra"
+        MOVIE_FILENAME=${MOVIE_FILENAME}_iframes
+    else
+        IFRAME_PARAMS=""
+    fi
+    MOVIE_FILENAME=${MOVIE_FILENAME}.mpg
+    ffmpeg ${AUDIO_PARAMS} -t ${MOVIE_LENGTH_IN_SECONDS}.04 -b 500 -i testmovies/tmp/${MOVIE_WIDTH}x${MOVIE_HEIGHT}_frame%07d.png ${IFRAME_PARAMS} -y -r ${FRAMERATE} -vcodec mpeg2video ${MOVIE_FILENAME} 
 }
 
 createImages 
-createMovie 1 true
-createMovie 10 true
-createMovie 60 true
-createMovie 1 false
-createMovie 10 false
-createMovie 60 false
+
+createMovie 1 true true
+#createMovie 10 true true
+#createMovie 60 true true
+#createMovie 1 true false
+#createMovie 10 true false
+#createMovie 60 true false
+
+#createMovie 1 false true
+#createMovie 10 false true
+#createMovie 60 false true
+#createMovie 1 false false
+#createMovie 10 false false
+#createMovie 60 false false
 
 
 # Cleanup temporary files
@@ -57,4 +73,5 @@ then
     echo "removing current images"
     rm testmovies/tmp/*
 fi
+echo done
 
