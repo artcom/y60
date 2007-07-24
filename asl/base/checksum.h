7f89d2c71dcc0acb197e948c074ecda024d4065f
gunnar <gunnar@f3027e6b-e3f5-0310-a8e1-edde99107618>
2007-07-24 14:19:21 +0000

    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@12540 f3027e6b-e3f5-0310-a8e1-edde99107618

--- asl/base/checksum.h
+++ asl/base/checksum.h
@@ -10,36 +10,6 @@
 // __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
 //
 //
-// Description:  Cross Platform Error Code 
-//
-// Last Review: pavel 30.11.2005 
-//
-//  review status report: (perfect, ok, fair, poor, disaster)
-//    usefullness            : ok
-//    formatting             : ok
-//    documentation          : poor
-//    test coverage          : poor
-//    names                  : fair
-//    style guide conformance: ok
-//    technical soundness    : ok
-//    dead code              : ok
-//    readability            : ok
-//    understandabilty       : ok
-//    interfaces             : ok
-//    confidence             : ok
-//    integration            : fair
-//    dependencies           : ok
-//    cheesyness             : fair
-//
-//    overall review status  : ok
-//
-//    recommendations:
-//       - make a documentation
-//       - make general use of this throughout the whole framework, at least throughout asl
-//       - rename LAST_ERROR_TYPE to something style-guide conforming
-//       - pur errorDescription() implementation into a .cpp file
-// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
-*/
 
 #ifndef _included_asl_checksum_
 #define _included_asl_checksum_
