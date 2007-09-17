a710b4d4a1c7029abec2781dcbf0fc48e1e75828
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2007-09-17 13:56:26 +0000

    Removed missing list for font options - all implemented.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@13288 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/Cairo/JSCairoFontOptions.cpp
+++ Y60/components/Cairo/JSCairoFontOptions.cpp
@@ -36,31 +36,6 @@ toString(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
     return JS_TRUE;
 }
 
-// MISSING:
-// void        cairo_font_options_set_antialias
-//                                             (cairo_font_options_t *options,
-//                                              cairo_antialias_t antialias);
-// cairo_antialias_t cairo_font_options_get_antialias
-//                                             (const cairo_font_options_t *options);
-// enum        cairo_subpixel_order_t;
-// void        cairo_font_options_set_subpixel_order
-//                                             (cairo_font_options_t *options,
-//                                              cairo_subpixel_order_t subpixel_order);
-// cairo_subpixel_order_t cairo_font_options_get_subpixel_order
-//                                             (const cairo_font_options_t *options);
-// enum        cairo_hint_style_t;
-// void        cairo_font_options_set_hint_style
-//                                             (cairo_font_options_t *options,
-//                                              cairo_hint_style_t hint_style);
-// cairo_hint_style_t cairo_font_options_get_hint_style
-//                                             (const cairo_font_options_t *options);
-// enum        cairo_hint_metrics_t;
-// void        cairo_font_options_set_hint_metrics
-//                                             (cairo_font_options_t *options,
-//                                              cairo_hint_metrics_t hint_metrics);
-// cairo_hint_metrics_t cairo_font_options_get_hint_metrics
-//                                             (const cairo_font_options_t *options);
-
 static JSBool
 setAntialias(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
     DOC_BEGIN("");
