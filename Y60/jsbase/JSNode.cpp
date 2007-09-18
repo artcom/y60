d0f66d339f826236587aecf154b69f5eda7dda9f
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2007-09-18 14:39:03 +0000

    - removed warning
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@13317 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/jsbase/JSNode.cpp
+++ Y60/jsbase/JSNode.cpp
@@ -677,9 +677,11 @@ getElementById(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rva
         return JS_FALSE;
     }
     dom::NodePtr myRetNode = myNode->getElementById(myId, myIdAttribute);
+    /*
     if (!myRetNode) {
         AC_DEBUG << "JSNode::getElementById: node with id '" << myId << "' not found by node " << *myNode->cloneNode(dom::Node::SHALLOW);
     }
+    */
     *rval = as_jsval(cx, myRetNode);
     return JS_TRUE;
 }
