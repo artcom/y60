b30432f613ed3bb43c1959f49eb2a9b785b9f214
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2007-10-15 16:51:04 +0000

    (Y60) Added copyright header and a comment to acspidermonkey. Added acspidermonkey to build.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@13775 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/products/acspidermonkey/main.c
+++ Y60/products/acspidermonkey/main.c
@@ -1,4 +1,14 @@
+//=============================================================================
+// Copyright (C) 2007, ART+COM AG Berlin
+//
+// These coded instructions, statements, and computer programs contain
+// unpublished proprietary information of ART+COM AG Berlin, and
+// are copy protected by law. They may not be disclosed to third parties
+// or copied or duplicated in any form, in whole or in part, without the
+// specific, prior written permission of ART+COM AG Berlin.
+//=============================================================================
 
+// This is an internal function from spidermonkey/js.c.
 extern int mymain(int argc, char **argv);
 
 int
@@ -6,4 +16,3 @@ main(int argc, char **argv)
 {
   return mymain(argc, argv);
 }
-
