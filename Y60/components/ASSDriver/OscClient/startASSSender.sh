70aee6e657e44d46e93a2ec55b9a8fed29f7a9b1
rodja <rodja@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-05-16 09:28:20 +0000

    loop
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@17553 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/ASSDriver/OscClient/startASSSender.sh
+++ Y60/components/ASSDriver/OscClient/startASSSender.sh
@@ -6,4 +6,9 @@ export LD_LIBRARY_PATH=$Y60:$LD_LIBRARY_PATH
 
 echo $LD_LIBRARY_PATH
 
-y60 client.js
+LOOP="1"
+while [ $LOOP == "1" ]; do
+    y60 client.js
+    sleep 1
+    echo "restarting"
+done;
