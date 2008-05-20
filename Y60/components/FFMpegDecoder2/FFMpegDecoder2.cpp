e3bdc82490e6cadcd84ac1817a39067bb3ebf15f
robert <robert@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-05-20 13:08:45 +0000

    increased cachesize to 128 frames
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@17624 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/FFMpegDecoder2/FFMpegDecoder2.cpp
+++ Y60/components/FFMpegDecoder2/FFMpegDecoder2.cpp
@@ -48,7 +48,7 @@ EXPORT asl::PlugInBase * y60FFMpegDecoder2_instantiatePlugIn(asl::DLHandle myDLH
 
 namespace y60 {
 
-    const unsigned FFMpegDecoder2::FRAME_CACHE_SIZE = 64;//8;
+    const unsigned FFMpegDecoder2::FRAME_CACHE_SIZE = 128;//64;//8;
     const double FFMpegDecoder2::AUDIO_BUFFER_SIZE = 0.5;
 
     asl::Block FFMpegDecoder2::_myResampledSamples(AVCODEC_MAX_AUDIO_FRAME_SIZE);
