d2b381c0c3c43b4c7b790b5b64052886651ede6a
rodja <rodja@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-06-30 18:25:40 +0000

    special bash "trap" to kill all jobs of the current script; neccessary if you "kill" a script
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@18302 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/ASSDriver/OscClient/startASSSender.sh
+++ Y60/components/ASSDriver/OscClient/startASSSender.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+function onExit {
+         echo killing all jobs
+         kill `jobs -p`
+}
+
+# necessary to remove all jobs of this shell if we get a SIGTERM or
+# similar
+trap "onExit" EXIT
+
+
 Y60=`pwd`/..
 export PATH=$Y60:$PATH
 export LD_LIBRARY_PATH=$Y60:$LD_LIBRARY_PATH
