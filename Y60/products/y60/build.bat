f81a9bd768a131d8b64d260f7450032d5b12b989
hendrik <hendrik@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-07-04 11:44:16 +0000

    added "--noprofile" to bash options
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@18357 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/products/y60/build.bat
+++ Y60/products/y60/build.bat
@@ -1,3 +1,3 @@
 call "C:\Programme\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
 set CHERE_INVOKING=1
-c:\cygwin\bin\bash.exe --login ./build.sh
+c:\cygwin\bin\bash.exe --login --noprofile ./build.sh
