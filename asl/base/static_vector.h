c05f19a89842b5b4f66e73b90b182e418dcaf9dd
hendrik <hendrik@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-08-15 15:37:12 +0000

    tabs to spaces
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@18766 f3027e6b-e3f5-0310-a8e1-edde99107618

--- asl/base/static_vector.h
+++ asl/base/static_vector.h
@@ -32,89 +32,89 @@ namespace asl {
     /*! \addtogroup aslbase */
     /* @{ */
     
-	/*! vector with compile-time fixed capacity
-	** 
-	** This behaves just like 'std::vector<>' except for the following 
-	** deviations: 
-	** - it doesn't use dynamic memory, the capacity is determined at compile-
-	**   time; there's a static member 'static_max_size' to find out about it
-	** - its 'swap()' member function is O(n) and might throw, if the 
-	**   'value_type's cctor throws
-	** 
-	** note  see std::vector documentation for all other members
-	*/
-	template< typename T, std::size_t MAX_SIZE >
-	class static_vector {
-		public:
-			// type definitions
-			typedef T                                     value_type;
-			typedef value_type*                           pointer;
-			typedef const value_type*                     const_pointer;
-			typedef value_type&                           reference;
-			typedef const value_type&                     const_reference;
-			typedef pointer                               iterator;
-			typedef const_pointer                         const_iterator;
-			typedef std::reverse_iterator<iterator>       reverse_iterator;
-			typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-			typedef std::size_t                           size_type;
-			typedef std::ptrdiff_t                        difference_type;
+    /*! vector with compile-time fixed capacity
+    ** 
+    ** This behaves just like 'std::vector<>' except for the following 
+    ** deviations: 
+    ** - it doesn't use dynamic memory, the capacity is determined at compile-
+    **   time; there's a static member 'static_max_size' to find out about it
+    ** - its 'swap()' member function is O(n) and might throw, if the 
+    **   'value_type's cctor throws
+    ** 
+    ** note  see std::vector documentation for all other members
+    */
+    template< typename T, std::size_t MAX_SIZE >
+    class static_vector {
+        public:
+            // type definitions
+            typedef T                                     value_type;
+            typedef value_type*                           pointer;
+            typedef const value_type*                     const_pointer;
+            typedef value_type&                           reference;
+            typedef const value_type&                     const_reference;
+            typedef pointer                               iterator;
+            typedef const_pointer                         const_iterator;
+            typedef std::reverse_iterator<iterator>       reverse_iterator;
+            typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+            typedef std::size_t                           size_type;
+            typedef std::ptrdiff_t                        difference_type;
 
-			// constructors/destructors
-			static_vector()
+            // constructors/destructors
+            static_vector()
                 : size_(0), elems_(reinterpret_cast<value_type*>(storage_)) {
             }
 
-			explicit static_vector(size_type count, const value_type& val = value_type())
-			    : size_(0), elems_(reinterpret_cast<value_type*>(storage_)) {
+            explicit static_vector(size_type count, const value_type& val = value_type())
+                : size_(0), elems_(reinterpret_cast<value_type*>(storage_)) {
                     assign(count,val);
             }
 
-			template< typename InpIt_ >
-			static_vector(InpIt_ it_beg, InpIt_ it_end)
+            template< typename InpIt_ >
+            static_vector(InpIt_ it_beg, InpIt_ it_end)
                 : size_(0), elems_(reinterpret_cast<value_type*>(storage_)) {
                 assign(it_beg,it_end);
             }
 
-			static_vector(const static_vector& rhs)
+            static_vector(const static_vector& rhs)
                 : size_(0), elems_(reinterpret_cast<value_type*>(storage_)) {
                 assign(rhs.begin(),rhs.end());
             }
 
-			~static_vector() {
+            ~static_vector() {
                 clear();
             }
 
-			// assignment
-			static_vector& operator=(const static_vector& rhs) {
+            // assignment
+            static_vector& operator=(const static_vector& rhs) {
                 assign(rhs.begin(),rhs.end());return*this;
             }
 
-			void
+            void
             assign(size_type count, const value_type& val);
 
-			template<class InpIt_>
-			void
+            template<class InpIt_>
+            void
             assign(InpIt_ it_beg, InpIt_ it_end);
 
-			// size/capacity
-			static const size_type            static_max_size = MAX_SIZE;
+            // size/capacity
+            static const size_type            static_max_size = MAX_SIZE;
 
-			bool
+            bool
             empty() const {
                 return 0==size();
             }
 
-			size_type
+            size_type
             max_size() const {
                 return static_max_size;
             }
 
-			size_type
+            size_type
             size() const {
                 return size_;
             }
 
-			void
+            void
             resize(size_type newsize, const value_type& val = value_type());
 
             size_type
@@ -122,164 +122,164 @@ namespace asl {
                 return max_size();
             }
 
-			void reserve(size_type count) {
+            void reserve(size_type count) {
                 check_size_(count);
             }
 
-			// data access
-			iterator
+            // data access
+            iterator
             begin() {
                 return elems_;
             }
 
-			const_iterator
+            const_iterator
             begin() const {
                 return elems_;
             }
 
-			iterator
+            iterator
             end() {
                 return elems_+size();
             }
 
-			const_iterator
+            const_iterator
             end() const {
                 return elems_+size();
             }
 
-			reverse_iterator
+            reverse_iterator
             rbegin() {
                 return reverse_iterator(end());
             }
 
-			const_reverse_iterator
+            const_reverse_iterator
             rbegin() const {
                 return const_reverse_iterator(end());
             }
 
-			reverse_iterator
+            reverse_iterator
             rend() {
                 return reverse_iterator(begin());
             }
 
-			const_reverse_iterator
+            const_reverse_iterator
             rend() const {
                 return const_reverse_iterator(begin());
             }
 
-			reference
+            reference
             operator[](size_type idx) {
                 return elems_[idx];
             }
 
-			const_reference
+            const_reference
             operator[](size_type idx)const {
                 return elems_[idx];
             }
 
-			reference
+            reference
             at(size_type idx) {
                 check_index_(idx); return operator[](idx);
             }
 
-			const_reference
+            const_reference
             at(size_type idx) const {
                 check_index_(idx); return operator[](idx);
             }
                                                                        
-			reference
+            reference
             front() {
                 return elems_[0];
             }
 
-			const_reference
+            const_reference
             front() const {
                 return elems_[0];
             }
 
-			reference
+            reference
             back() {
                 return elems_[size()-1];
             }
 
-			const_reference
+            const_reference
             back() const {
                 return elems_[size()-1];
             }
 
-			// stack operations
-			void
+            // stack operations
+            void
             push_back(const value_type& val) {
                 insert(end(),val);
             }
 
-			void
+            void
             pop_back() {
                 erase(end()-1,end());
             }
 
-			// erasure
-			void
+            // erasure
+            void
             clear() {
                 erase(begin(),end());
             }
 
-			void
+            void
             erase(iterator it_where) {
                 destroy_( std::copy(it_where+1,end(),it_where), end() );
             }
 
-			void
+            void
             erase(iterator it_beg, iterator it_end) 
             {
                 destroy_( std::copy(it_end,end(),it_beg), end() );
             }
 
-			// insertion
-			iterator
+            // insertion
+            iterator
             insert(iterator it_where, const value_type& val) {
                 insert(it_where,static_cast<size_type>(1),val);return it_where;
             }
 
-			void
+            void
             insert(iterator it_where, size_type count, const value_type& val);
 
-			template< typename InpIt_ >
-			void
+            template< typename InpIt_ >
+            void
             insert(iterator it_where, InpIt_ it_beg, InpIt_ it_end ) {
                 insert_(it_where,it_beg,it_end,typename std::iterator_traits<InpIt_>::iterator_category());
             }
 
             // swap
 
-			/*! 
+            /*! 
             ** note Contrary to std::vector, this has linear complexity!
             */
-			void
+            void
             swap(static_vector& rhs) {
-				static_vector& smaller = size()<rhs.size() ? *this : rhs;
-				static_vector& bigger  = size()<rhs.size() ? rhs : *this;
-				swap_(smaller,bigger);
-			}
+                static_vector& smaller = size()<rhs.size() ? *this : rhs;
+                static_vector& bigger  = size()<rhs.size() ? rhs : *this;
+                swap_(smaller,bigger);
+            }
 
-		private:
-			size_type                         size_;
-			char                              storage_[ sizeof(value_type[MAX_SIZE]) ];
-			value_type*                       elems_;
+        private:
+            size_type                         size_;
+            char                              storage_[ sizeof(value_type[MAX_SIZE]) ];
+            value_type*                       elems_;
 
-			static
+            static
             void
             storage_exhausted_() {
                 throw std::range_error("static_vector storage exhausted");
             }
 
-			static
+            static
             void
             invalid_index_() {
                 throw std::range_error("static_vector invalid index");
             }
 
-			static
+            static
             void
             check_size_(size_type size) {
                 if(size>static_max_size) {
@@ -287,93 +287,93 @@ namespace asl {
                 }
             }
 
-			void
+            void
             check_index_(size_type idx) const {
                 if(idx>=size()) {
                     invalid_index_();
                 }
             }
 
-			void
+            void
             make_room_(iterator it_where, size_type count);
 
-			template< typename InpIt_ >
-			void
+            template< typename InpIt_ >
+            void
             insert_(pointer it_where, InpIt_ it_beg, InpIt_ it_end, std::input_iterator_tag) {
-				for( ; it_beg != it_end; ++it_beg, ++it_where ) {
-					it_where = insert(it_where,*it_beg);
-				}
-			}
+                for( ; it_beg != it_end; ++it_beg, ++it_where ) {
+                    it_where = insert(it_where,*it_beg);
+                }
+            }
 
-			template< typename FwdIt_, class IterTag_ >
-			void
+            template< typename FwdIt_, class IterTag_ >
+            void
             insert_(pointer it_where, FwdIt_ it_beg, FwdIt_ it_end, IterTag_);
 
-			void
+            void
             append_(size_type count, const value_type& val);
 
-			template<class FwdIt_, class IterTag_>
+            template<class FwdIt_, class IterTag_>
             void
             append_(FwdIt_ it_beg, FwdIt_ it_end, IterTag_);
-			
-			template<class InpIt_>
-			void
+            
+            template<class InpIt_>
+            void
             append_(InpIt_ it_beg, InpIt_ it_end, std::input_iterator_tag);
-			
-			void
+            
+            void
             destroy_(pointer it_beg, pointer it_end) {for(;it_beg!=it_end;--size_)(--it_end)->~value_type();}
 
-			static
+            static
             void
             swap_(static_vector& smaller, static_vector& bigger);
 
-	};
+    };
 
-	// comparisons
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    // comparisons
+    template<class T, std::size_t MAX_SIZE>
+    inline
     bool
     operator==(const static_vector<T,MAX_SIZE>& lhs, const static_vector<T,MAX_SIZE>& rhs) {
         return std::equal(lhs.begin(), lhs.end(), rhs.begin());
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    template<class T, std::size_t MAX_SIZE>
+    inline
     bool
     operator< (const static_vector<T,MAX_SIZE>& lhs, const static_vector<T,MAX_SIZE>& rhs) {
         return std::lexicographical_compare(lhs.begin(),lhs.end(),rhs.begin(),rhs.end());
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    template<class T, std::size_t MAX_SIZE>
+    inline
     bool
     operator!=(const static_vector<T,MAX_SIZE>& lhs, const static_vector<T,MAX_SIZE>& rhs) {
         return !(lhs==rhs);
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    template<class T, std::size_t MAX_SIZE>
+    inline
     bool
     operator> (const static_vector<T,MAX_SIZE>& lhs, const static_vector<T,MAX_SIZE>& rhs) {
         return rhs<lhs;
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    template<class T, std::size_t MAX_SIZE>
+    inline
     bool
     operator<=(const static_vector<T,MAX_SIZE>& lhs, const static_vector<T,MAX_SIZE>& rhs) {
         return !(rhs<lhs);
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    template<class T, std::size_t MAX_SIZE>
+    inline
     bool
     operator>=(const static_vector<T,MAX_SIZE>& lhs, const static_vector<T,MAX_SIZE>& rhs) {
         return !(lhs<rhs);
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	inline
+    template<class T, std::size_t MAX_SIZE>
+    inline
     void
     swap (static_vector<T,MAX_SIZE>& lhs, static_vector<T,MAX_SIZE>& rhs) {
         lhs.swap(rhs);
@@ -382,7 +382,7 @@ namespace asl {
     ////////////////////////////////////////////////////////////////////////////
     // implementations
 
-	template<class T, std::size_t MAX_SIZE>
+    template<class T, std::size_t MAX_SIZE>
     inline
     void
     static_vector<T,MAX_SIZE>::assign(size_type count, const value_type& val) {
@@ -398,12 +398,12 @@ namespace asl {
         }
     }
 
-	template<class T, std::size_t MAX_SIZE>
-	template<class InpIt_>
+    template<class T, std::size_t MAX_SIZE>
+    template<class InpIt_>
     inline
     void
     static_vector<T,MAX_SIZE>::assign(InpIt_ it_beg, InpIt_ it_end) {
-		pointer it = begin();
+        pointer it = begin();
         while( it_beg!=it_end && it!=end() ) {
             *it++ = *it_beg++;
         }
@@ -412,9 +412,9 @@ namespace asl {
         } else {
             erase(it,end());
         }
-	}
+    }
 
-	template<class T, std::size_t MAX_SIZE>
+    template<class T, std::size_t MAX_SIZE>
     inline
     void
     static_vector<T,MAX_SIZE>::resize(size_type newsize, const value_type& val) {
