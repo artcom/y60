8f440cd483be7fd60d5294c85fcaac6426a3f41d
sebastianh <sebastianh@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-08-27 10:33:21 +0000

    (y60/js) added GUI and subdirs into Makedist.pm
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@18883 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/js/Makedist.pm
+++ Y60/js/Makedist.pm
@@ -2,7 +2,9 @@ package Makedist;
 use MakedistTools;
 
 sub runLocalMakedist($) {
-    copyDirectory(".", "$_[0]", (".js", ".glade", ".gladep"));      
+    copyDirectory(".",   "$_[0]", (".js", ".glade", ".gladep"));      
+    copyDirectory("GUI", "$_[0]/GUI", (".js"));
+    copyDirectory("GUI/Layers", "$_[0]/GUI/Layers", (".js"));
+    copyDirectory("GUI/Animation", "$_[0]/GUI/Animation", (".js"));
 }
-
 1;
