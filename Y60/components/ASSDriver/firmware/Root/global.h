83656aa3da25f27550e1790307fcb07be69d676c
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-09-30 20:12:40 +0000

    (ASS) Fixed order of global.h to match global.c.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@19266 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/ASSDriver/firmware/Root/global.h
+++ Y60/components/ASSDriver/firmware/Root/global.h
@@ -8,55 +8,65 @@ extern uint8_t g_scanFrequency;
 extern uint8_t g_gridSpacing;
 extern uint8_t g_ID;
 extern uint8_t g_tFrequency;
-extern uint8_t g_mode;
-extern uint8_t g_status;
-extern uint8_t g_errorState;
-extern uint8_t g_mainTimer;
-extern uint8_t g_DIPSwitch;
-extern uint8_t g_UARTBytesReceived;
-extern uint8_t g_arg1;
-extern uint8_t g_arg2;
+extern uint8_t g_BaudRateFactor;
+// txVoltage
+
+extern uint8_t g_maxMinLevelRequest;
+extern uint8_t g_maxLevel;
+extern uint8_t g_minLevel;
 extern uint8_t g_PMinX;
 extern uint8_t g_PMinY;
 extern uint8_t g_PMaxX;
 extern uint8_t g_PMaxY;
-extern uint8_t g_BaudRateFactor;
-extern uint8_t g_UART_mode;
-extern uint8_t g_UART_select;
-extern uint8_t g_dataTransmitMode;
-extern uint8_t g_T1[];
 
-extern uint8_t g_rowBuffer[];
-extern uint8_t g_colAverage[];
+extern uint8_t  g_XYLevelRequest;
+extern uint8_t  g_XYLevel;
 
+extern uint8_t g_DIPSwitch;
+
+extern uint8_t g_rowTransmissionBuffer[];
 extern uint8_t g_RTB_Write_Pointer;
 extern uint8_t g_RTB_Read_Pointer;
 extern uint8_t g_RTB_Filled;
 extern uint8_t g_PacketCounter;
 
-extern uint8_t g_rowTransmissionBuffer[];
+extern uint8_t g_UART_mode;
+extern uint8_t g_UART_select;
+extern uint8_t g_UARTBytesReceived;
 
-extern uint8_t g_currentRow;
+extern uint8_t g_NextCommand;
+extern uint8_t g_arg1;
+extern uint8_t g_arg2;
 
 extern uint16_t g_scanPeriod;
 extern uint16_t g_deltaTRow;
+
 extern uint16_t g_FrameNumber;
 
+extern uint8_t g_dataTransmitMode;
+
+extern uint8_t g_rowBuffer[];
+
+extern uint8_t g_colAverage[];
+extern uint8_t g_T1[];
+
+extern uint8_t g_mode;
+
+extern uint8_t g_errorState;
+
+extern uint8_t g_mainTimer;
+
+extern uint8_t g_currentRow;
 extern uint8_t g_readState;
 
-extern uint8_t g_SystemCalibrated;
 extern uint8_t g_TaraRequest;
+extern uint8_t g_SystemCalibrated;
 
 extern uint8_t g_ConfigMode;
-extern uint8_t g_NextCommand;
-
-extern uint8_t  g_maxMinLevelRequest;
-extern uint8_t  g_XYLevelRequest;
-extern uint8_t  g_maxLevel;
-extern uint8_t  g_minLevel;
-extern uint8_t  g_XYLevel;
 
 extern uint16_t ADCValue_C0;
 extern uint16_t ADCValue_C1;
+
+extern uint8_t g_status;
 
 #endif
