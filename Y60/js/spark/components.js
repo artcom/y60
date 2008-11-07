a2fbba317cf6a291594f11c522591bbe3fec6fc3
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-11-07 14:10:38 +0000

    (SPARK) Merge template instance dom into one common layout dom.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@19714 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/js/spark/components.js
+++ Y60/js/spark/components.js
@@ -190,6 +190,7 @@ spark.Container.Constructor = function(Protected) {
 
     Public.addChild = function(theChild) {
         _myChildArray.push(theChild);
+        Public.node.appendChild(theChild.node);
         if(theChild.name) {
             _myChildMap[theChild.name] = theChild;
         }
