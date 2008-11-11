1d3f9753041c4a68dc3a35752fc603a7055957fd
robert <robert@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-11-11 10:06:43 +0000

    altered makedist to be run from a project directory
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@19773 f3027e6b-e3f5-0310-a8e1-edde99107618

--- Y60/components/SynergyServer/Makedist.pm
+++ Y60/components/SynergyServer/Makedist.pm
@@ -12,7 +12,6 @@ sub runLocalMakedist($) {
 
     my $theDistDir = $_[0];
 
-# Install special files
     copyIfMoreRecent("settings.xml", $theDistDir);
 
     copyIfMoreRecent("$ENV{PRO}/lib/libASSEventSourceOPT.so", $theDistDir);
@@ -20,12 +19,17 @@ sub runLocalMakedist($) {
     copyIfMoreRecent("$ENV{PRO}/lib/libEventLoopOPT.so", $theDistDir);
     copyIfMoreRecent("$ENV{PRO}/lib/libSynergyServerOPT.so", $theDistDir);
     copyIfMoreRecent("$ENV{PRO}/lib/liboscpackOPT.so", $theDistDir);
-    copyIfMoreRecent("SynergyEventHandler.js", $theDistDir);
-    copyIfMoreRecent("ClickOnPointHandler.js", $theDistDir);
-    copyIfMoreRecent("RelativeMouseHandler.js", $theDistDir);
-    copyIfMoreRecent("ClickWithSecondHand.js", $theDistDir);
-    copyIfMoreRecent("demo.js", $theDistDir);
-    copyIfMoreRecent("start_dist.sh", $theDistDir);
+    copyIfMoreRecent("$ENV{PRO}/src/Y60/components/SynergyServer/SynergyEventHandler.js", 
+                     $theDistDir);
+    copyIfMoreRecent("$ENV{PRO}/src/Y60/components/SynergyServer/ClickOnPointHandler.js", 
+                     $theDistDir);
+    copyIfMoreRecent("$ENV{PRO}/src/Y60/components/SynergyServer/RelativeMouseHandler.js", 
+                     $theDistDir);
+    copyIfMoreRecent("$ENV{PRO}/src/Y60/components/SynergyServer/ClickWithSecondHand.js", 
+                     $theDistDir);
+    copyIfMoreRecent("$ENV{PRO}/src/Y60/components/SynergyServer/demo.js", $theDistDir);
+    copyIfMoreRecent("$ENV{PRO}/src/Y60/components/SynergyServer/start_dist.sh", 
+                     $theDistDir);
 
     depends("Y60/products/y60");
 }
