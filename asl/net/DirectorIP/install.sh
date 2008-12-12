07997f2a380cc65444fa19117bccfbc351c59be8
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2008-12-12 15:21:56 +0000

    (PRO60) Removed DirectorIP and DirectorTCP from aslnet. Obsolete.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20367 f3027e6b-e3f5-0310-a8e1-edde99107618

--- asl/net/DirectorIP/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ !(-n $1) ]]
-then
-  echo "Usage: ./install.sh DBG|OPT"
-  echo "Note: Make sure director is installed where this script expects it. Or change the script."
-else  
-  cp -v $PRO/src/asl/net/DirectorIP/o.ANT.WIN.$1/aslDirectorIP$1.dll "/cygdrive/c/Programme/Macromedia/Director 8.5G/Xtras/DirectorIP.x32"
-fi
