7aef8e29a4d126fadfa0a4eb6a86f4ffccae80a7
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-01-06 14:17:03 +0000

    CMake: depend on pthreadVC on WIN32
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20565 f3027e6b-e3f5-0310-a8e1-edde99107618

--- asl/ASLDependencies.cmake
+++ asl/ASLDependencies.cmake
@@ -4,12 +4,16 @@ include(FindPkgConfig)
 find_package(ZLIB)
 
 pkg_search_module(GLIB2    REQUIRED glib-2.0)
-pkg_search_module(CURL     REQUIRED libcurl)
-pkg_search_module(OPENEXR  REQUIRED OpenEXR)
+find_package(CURL)
+find_package(OPENEXR)
 
 if(LINUX)
     pkg_search_module(ALSA REQUIRED alsa)
 endif(LINUX)
+
+if(WIN32)
+    find_package( PTHREADVC )
+endif(WIN32)
 
 if(OSX)
     find_library(CARBON     Carbon)
