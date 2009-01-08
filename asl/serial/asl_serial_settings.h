55fdcb7b2bc200020e72ab976a96ee00c5a9db0c
katjae <katjae@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-01-08 15:46:10 +0000

    windll: helper-header
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20612 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ asl/serial/asl_serial_settings.h
@@ -0,0 +1,12 @@
+#ifndef ASL_SERIAL_SETTING_INCLUDED
+#define ASL_SERIAL_SETTING_INCLUDED
+
+#include <asl/base/settings.h>
+
+#ifdef aslserial_EXPORTS
+#   define ASL_SERIAL_EXPORT AC_DLL_EXPORT
+#else
+#   define ASL_SERIAL_EXPORT AC_DLL_IMPORT
+#endif
+
+#endif // ASL_SERIAL_SETTING_INCLUDED
