152bbf4d91ddff9bf745c4b84f55b08979ae161a
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-01-09 23:17:22 +0000

    (PRO60) Fixed spidermonkey definition issue.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20662 f3027e6b-e3f5-0310-a8e1-edde99107618

--- netsrc/spidermonkey/SpidermonkeyDefinitions.cmake
+++ netsrc/spidermonkey/SpidermonkeyDefinitions.cmake
@@ -2,13 +2,13 @@
 if(UNIX)
     if(APPLE)
         # Apple Mac OS X
-        add_definitions(-DXP_UNIX)
+        set(SPIDERMONKEY_DEFINITIONS -DXP_UNIX)
     else(APPLE)
         # Linux
-        add_definitions(-DXP_UNIX -DHAVE_VA_COPY -DVA_COPY=va_copy)
+        set(SPIDERMONKEY_DEFINITIONS -DXP_UNIX -DHAVE_VA_COPY -DVA_COPY=va_copy)
     endif(APPLE)
 else(UNIX)
     # Microsoft Windows
-    add_definitions(-DXP_WIN -D_WINDOWS -D_X86_=1 -DJSFILE)
+    set(SPIDERMONKEY_DEFINITIONS -DXP_WIN -D_WINDOWS -D_X86_=1 -DJSFILE)
 endif(UNIX)
 
