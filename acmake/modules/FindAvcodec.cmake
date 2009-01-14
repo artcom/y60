b478ff42f7fb63f6219e3e92b20cc1f05b179e92
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-01-14 12:33:17 +0000

    Ffmpeg lib handling: avformat and avcodec require avutil
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20722 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/FindAvcodec.cmake
+++ acmake/modules/FindAvcodec.cmake
@@ -2,6 +2,8 @@ if(WIN32)
     find_path( AVCODEC_INCLUDE_DIR avcodec.h
             PATH_SUFFIXES libavcodec ffmpeg)
     find_library( AVCODEC_LIBRARIES avcodec libavcodec)
+    find_library( AVUTIL_LIBRARY avutil libavutil)
+    list( APPEND AVFORMAT_LIBRARIES ${AVUTIL_LIBRARY} )
 else(WIN32)
     pkg_search_module(AVCODEC REQUIRED libavcodec)
 endif(WIN32)
--- acmake/modules/FindAvformat.cmake
+++ acmake/modules/FindAvformat.cmake
@@ -2,6 +2,8 @@ if(WIN32)
     find_path( AVFORMAT_INCLUDE_DIR avformat.h
             PATH_SUFFIXES libavformat ffmpeg)
     find_library( AVFORMAT_LIBRARIES avformat libavformat)
+    find_library( AVUTIL_LIBRARY avutil libavutil)
+    list( APPEND AVFORMAT_LIBRARIES ${AVUTIL_LIBRARY} )
 else(WIN32)
     pkg_search_module(AVFORMAT REQUIRED libavformat)
 endif(WIN32)
