8841a9743cc2e0e95aff090272b415d3ceeb43fc
pavel <pavel@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-01-17 00:35:58 +0000

    fixed OSX Cg finder on OSX
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20798 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/FindNvidiaCg.cmake
+++ acmake/modules/FindNvidiaCg.cmake
@@ -2,11 +2,13 @@
 #find_package( OpenGL )
 find_library( NVIDIA_CG_LIBRARIES NAMES Cg cg )
 if(APPLE AND UNIX AND NVIDIA_CG_LIBRARIES MATCHES ".*\\.framework/?$")
-        set( NVIDIA_CG ${NVIDIA_CG_LIBRARIES} )
+    set( NVIDIA_CG_PACKAGE_VARS NVIDIA_CG_LIBRARIES )
+    set( NVIDIA_CG ${NVIDIA_CG_LIBRARIES} )
     # TODO: handle non-framework versions on OSX?
 else(APPLE AND UNIX AND NVIDIA_CG_LIBRARIES MATCHES ".*\\.framework/?$")
+    set( NVIDIA_CG_PACKAGE_VARS NVIDIA_CG_LIBRARIES NVIDIA_CG_INCLUDE_DIRS )
     find_path( NVIDIA_CG_INCLUDE_DIRS cg.h 
-            PATH_SUFFIXES Cg GL )
+               PATH_SUFFIXES Cg GL )
     get_filename_component( NVIDIA_CG_LIBRARY_DIRS
             "${NVIDIA_CG_LIBRARIES}" PATH )
     set(NVIDIA_CG_LIBRARIES Cg CgGL ${OPENGL_LIBRARIES})
@@ -16,4 +18,5 @@ else(APPLE AND UNIX AND NVIDIA_CG_LIBRARIES MATCHES ".*\\.framework/?$")
 endif(APPLE AND UNIX AND NVIDIA_CG_LIBRARIES MATCHES ".*\\.framework/?$")
 
 find_package_handle_standard_args(NVIDIA_CG DEFAULT_MSG
-        NVIDIA_CG_LIBRARIES NVIDIA_CG_INCLUDE_DIRS)
+        ${NVIDIA_CG_PACKAGE_VARS})
+
