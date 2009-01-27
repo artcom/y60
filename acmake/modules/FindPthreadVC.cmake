ba731807f51fbf0686f9f2b84c348799f8ac5a8f
katjae <katjae@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-01-27 12:29:00 +0000

    cmake: find pthreads debug library
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@20928 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/FindPthreadVC.cmake
+++ acmake/modules/FindPthreadVC.cmake
@@ -4,7 +4,8 @@ mark_as_advanced( PTHREADVC_INCLUDE_DIR )
 
 
 find_library(PTHREADVC_LIBRARY pthreadVC)
-mark_as_advanced( PTHREADVC_LIBRARY )
+find_library(PTHREADVC_LIBRARY_D pthreadVCd)
+mark_as_advanced( PTHREADVC_LIBRARY PTHREADVC_LIBRARY_D )
 
 include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( PTHREADVC DEFAULT_MSG PTHREADVC_LIBRARY
@@ -12,6 +13,7 @@ find_package_handle_standard_args( PTHREADVC DEFAULT_MSG PTHREADVC_LIBRARY
 
 if( PTHREADVC_FOUND )
     set( PTHREADVC_LIBRARIES ${PTHREADVC_LIBRARY} )
+    set( PTHREADVC_LIBRARIES_D ${PTHREADVC_LIBRARY_D} )
     set( PTHREADVC_INCLUDE_DIRS ${PTHREADVC_INCLUDE_DIR} )
 else( PTHREADVC_FOUND )
     set( PTHREADVC_LIBRARIES )
