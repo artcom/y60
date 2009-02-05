d16b8100cfd3849f5ea6852043ece8fab7638856
hendrik <hendrik@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-05 18:19:16 +0000

    further improvments in continuous build script
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21161 f3027e6b-e3f5-0310-a8e1-edde99107618

--- continuous_build.sh
+++ continuous_build.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-#set -e
-#set -x
+set -e
+set -x
 set -u
 
 BUILD_DIR=_build
+UNAME_OS=`uname -s`
+
+case "$UNAME_OS" in
+    Cygwin* | cygwin* | CYGWIN* | MINGW*)
+		CMAKE_GENERATOR="Visual Studio 9 2008"
+		MAKE_COMMAND='vcbuild PRO60.sln Release|Win32'
+        ;;
+    Darwin*)
+		CMAKE_GENERATOR="Unix Makefiles"
+		MAKE_COMMAND=make
+        ;;
+    Linux*)
+		CMAKE_GENERATOR="Unix Makefiles"
+		MAKE_COMMAND=make
+        ;;
+	*)
+		echo What?
+		;;
+esac
+
 
 echo configuring cmake build
 
@@ -18,14 +38,14 @@ if [ ! -d $BUILD_DIR ]; then
 fi
 cd $BUILD_DIR
 
-cmake ../src
+#cmake -G "$CMAKE_GENERATOR" ../src
 CMAKE_RESULT=$?
 if [ $CMAKE_RESULT -gt 0 ]; then
     echo cmake failed
     exit 2
 fi
 
-make
+$MAKE_COMMAND
 MAKE_RESULT=$?
 if [ $MAKE_RESULT -gt 0 ]; then
     echo make failed
