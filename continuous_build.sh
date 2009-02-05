be4698b8beab743a09a7352458876861096253cc
hendrik <hendrik@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-05 14:41:11 +0000

    improved minimal build script
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21156 f3027e6b-e3f5-0310-a8e1-edde99107618

--- continuous_build.sh
+++ continuous_build.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-set -e
-set -x
+#set -e
+#set -x
 set -u
+
+BUILD_DIR=_build
 
 echo configuring cmake build
 
 cd ..
-mkdir _build
-cd _build
+if [ ! -d $BUILD_DIR ]; then
+    mkdir $BUILD_DIR
+    MKDIR_RESULT=$?
+    if [ $MKDIR_RESULT -gt 0 ]; then
+        echo Cannot create $PWD/$BUILD_DIR
+        exit 3
+    fi
+fi
+cd $BUILD_DIR
 
 cmake ../src
+CMAKE_RESULT=$?
+if [ $CMAKE_RESULT -gt 0 ]; then
+    echo cmake failed
+    exit 2
+fi
 
 make
+MAKE_RESULT=$?
+if [ $MAKE_RESULT -gt 0 ]; then
+    echo make failed
+    exit 1
+fi
 
 echo done
+
+exit 0
