a1b773d4581819b26c73ae956705edcab4d4d99e
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-10 22:06:54 +0000

    CI: removed quotes from batch script
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21268 f3027e6b-e3f5-0310-a8e1-edde99107618

--- continuous_build.bat
+++ continuous_build.bat
@@ -1,23 +1,20 @@
 BREAK=ON
 
-set BUILD_DIR="_build"
-set CMAKE_GENERATOR="Visual Studio 9 2008"
-set VCBUILD_ARGS="PRO60.sln Release|Win32"
+set BUILD_DIR=_build
+set CMAKE_GENERATOR=Visual Studio 9 2008
+set VCBUILD_ARGS=PRO60.sln Release|Win32
 
-echo "=== Running on operating system %OS%"
-
+rem === Running on operating system %OS%
 
 if exist %BUILD_DIR% goto build_dir_exists
 
 mkdir %BUILD_DIR%
 
-echo === Build directory %BUILD_DIR% does not exist. Creating.
 :build_dir_exists
-
 cd %BUILD_DIR%
 
-echo === Configuring cmake build for %CMAKE_GENERATOR%
+rem === Configuring cmake build for %CMAKE_GENERATOR%
 cmake -G %CMAKE_GENERATOR% ..
 
-echo "=== Running vcbuild %VCBUILD_ARGS%"
+rem Running vcbuild %VCBUILD_ARGS%
 vcbuild %VCBUILD_ARGS%
