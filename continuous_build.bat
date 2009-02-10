7201b5f4125aed022980443110ebf180cd768134
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-10 23:24:43 +0000

    CI: fixed batch file
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21271 f3027e6b-e3f5-0310-a8e1-edde99107618

--- continuous_build.bat
+++ continuous_build.bat
@@ -2,9 +2,6 @@ BREAK=ON
 
 set BUILD_DIR=_build
 set CMAKE_GENERATOR=Visual Studio 9 2008
-set VCBUILD_ARGS="PRO60.sln Release|Win32"
-
-rem === Running on operating system %OS%
 
 if exist %BUILD_DIR% goto build_dir_exists
 
@@ -13,8 +10,5 @@ mkdir %BUILD_DIR%
 :build_dir_exists
 cd %BUILD_DIR%
 
-rem === Configuring cmake build for %CMAKE_GENERATOR%
-cmake -G %CMAKE_GENERATOR% ..
-
-rem Running vcbuild %VCBUILD_ARGS%
-vcbuild %VCBUILD_ARGS%
+cmake -G "%CMAKE_GENERATOR%" ..
+vcbuild PRO60.sln "Release|Win32"
