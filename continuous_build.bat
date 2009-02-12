595ab416dc761d2205f45a6a1351572299ba2cf4
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-12 01:03:53 +0000

    CI: more windows build script hacking
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21322 f3027e6b-e3f5-0310-a8e1-edde99107618

--- continuous_build.bat
+++ continuous_build.bat
@@ -1,24 +1,78 @@
 BREAK=ON
 
 set BUILD_DIR=_build
-set CMAKE_GENERATOR=Visual Studio 9 2008
+set USE_NMAKE=1
+
+if defined USE_NMAKE goto generator_nmake
+    set CMAKE_GENERATOR=Visual Studio 9 2008
+    goto generator_found
+:generator_nmake
+    set CMAKE_GENERATOR=NMake Makefiles
+:generator_found
+
+
+rem === Check environment variables ===========================================
+
+if defined %CONFIG% goto config_defined
+
+set CONFIG=Release
+
+:config_defined
+
+
+if defined %PLATFORM% goto platform_defined
+
+set PLATFORM=Win32
+
+:platform_defined
+
+
+if defined %NUMCORES% goto numcores_defined
+
+set NUMCORES=1
+
+:numcores_defined
+
+rem === Prepare build =========================================================
 
 if exist %BUILD_DIR% goto build_dir_exists
 
 mkdir %BUILD_DIR%
 
 :build_dir_exists
-cd %BUILD_DIR%
+
 
 call "%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
+
+cd %BUILD_DIR%
 
 cmake -G "%CMAKE_GENERATOR%" ..
 if ERRORLEVEL 1 goto error
 
-vcbuild PRO60.sln "Release|Win32"
+if defined USE_NMAKE goto build_with_namke
+rem === VC build ==============================================================
+
+vcbuild /M%NUMCORES% PRO60.sln "%CONFIG%|%PLATFORM%"
+rem nmake
+if ERRORLEVEL 1 goto error
+
+vcbuild TEST.vcproj
+if ERRORLEVEL 1 goto error
+
+exit 0
+
+rem === NMake build ===========================================================
+:build_with_namke
+
+nmake
+if ERRORLEVEL 1 goto error
+
+nmake test
 if ERRORLEVEL 1 goto error
 
 exit 0
 
 :error
+rem XXX
+pause
 exit 1
