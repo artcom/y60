23e6ed1566241bb390984e8b5385f72e1c4277ca
katjae <katjae@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-12 15:51:04 +0000

    added cinema-exporter installation (windows only)
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21345 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/FindC4D.cmake
+++ acmake/modules/FindC4D.cmake
@@ -1,32 +1,40 @@
 
 set(C4D_SDK $ENV{C4D_SDK})
+set(C4D_VERSION $ENV{C4D_VERSION})
 
-if(C4D_SDK)
+if(C4D_SDK AND C4D_VERSION)
 
-    find_path(C4D_INCLUDE_DIR c4d.h 
+    find_path(C4D_INCLUDE_DIR c4d.h
         PATHS ENV C4D_SDK
-        PATH_SUFFIXES c4d
-    )
-
-    set(C4D_INCLUDE_DIRS ${C4D_INCLUDE_DIR})
-
-    find_library(C4D_LIBRARIES NAMES c4d_api_v8OPT
-            PATHS ENV C4D_SDK
+        PATH_SUFFIXES _api
     )
     
-    find_library(C4D_LIBRARIES_D NAMES c4d_api_v8DBG
-            PATHS ENV C4D_SDK
+    find_path(C4D_GV_INCLUDE_DIR ge_mtools.h
+        PATHS ENV C4D_SDK
+        PATH_SUFFIXES _api/c4d_gv
     )
     
+    find_path(C4D_CUSTOMGUI_INCLUDE_DIR customgui_description.h
+        PATHS ENV C4D_SDK
+        PATH_SUFFIXES _api/c4d_customgui
+    )
     
-    if(WIN32)
-        set(C4D_DEFINITIONS -D__PC)
-    endif(WIN32)
-    if(OSX)
-        set(C4D_DEFINITIONS -D__MAC)
-    endif(OSX)
+    find_path(C4D_LIBS_INCLUDE_DIR lib_description.h
+        PATHS ENV C4D_SDK
+        PATH_SUFFIXES _api/c4d_libs
+    )
 
-endif(C4D_SDK)
+    set(C4D_INCLUDE_DIRS 
+            ${C4D_INCLUDE_DIR} 
+            ${C4D_GV_INCLUDE_DIR} 
+            ${C4D_CUSTOMGUI_INCLUDE_DIR}
+            ${C4D_LIBS_INCLUDE_DIR})
+
+    find_library(C4D_LIBRARIES NAMES c4d_api.lib _api_Win32_Release.lib c4d_api_v8OPT)
+    
+    find_library(C4D_LIBRARIES_D NAMES c4d_apid.lib _api_Win32_Debug.lib c4d_api_v8DBG)
+    
+endif(C4D_SDK AND C4D_VERSION)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(C4D DEFAULT_MSG C4D_INCLUDE_DIR C4D_LIBRARIES)
+find_package_handle_standard_args(C4D DEFAULT_MSG C4D_INCLUDE_DIRS C4D_LIBRARIES)
