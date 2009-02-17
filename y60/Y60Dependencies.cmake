7f93a06d9f1eb6198a7f756acf89e613d6d37494
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-17 23:07:45 +0000

    (CMake) Added gstreamer build. Also cleaned up Y60Dependencies.cmake a little bit.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21522 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/Y60Dependencies.cmake
+++ y60/Y60Dependencies.cmake
@@ -4,42 +4,56 @@ include(AcPlatform)
 
 include(FindPkgConfig)
 
+# art+com standard library
 list(APPEND REQ_PACKAGES ASL)
 
-# exported dependencies
+# netsrc dependencies
 list(APPEND REQ_PACKAGES Paintlib)
+list(APPEND REQ_PACKAGES ACSDLTTF)
+list(APPEND REQ_PACKAGES OSCPACK )
 
+# javascript implementation
 if (NOT Y60_WITH_TRACEMONKEY)
     list(APPEND REQ_PACKAGES Spidermonkey)
 else (NOT Y60_WITH_TRACEMONKEY)
     list(APPEND REQ_PACKAGES MOZJS)
 endif (NOT Y60_WITH_TRACEMONKEY)
 
-list(APPEND REQ_PACKAGES ACSDLTTF)
-
 # external dependencies
 list(APPEND REQ_PACKAGES SDL GLEW NvidiaCg OpenEXR GLIB2)
 list(APPEND OPT_PACKAGES Maya C4D)
 
+# gtk and consorts
 list(APPEND REQ_PKGCONFIG GTKMM gtkmm-2.4)
 list(APPEND REQ_PKGCONFIG GLADEMM libglademm-2.4)
 list(APPEND REQ_PKGCONFIG GTKGLEXT gtkglext-1.0)
+list(APPEND OPT_PKGCONFIG GSTREAMER gstreamer-0.10)
 
-if(WIN32)
-    list(APPEND REQ_PACKAGES PthreadVC)
-    list(APPEND REQ_PKGCONFIG CAIRO cairo-win32)
-elseif(LINUX)
-    list(APPEND REQ_PKGCONFIG CAIRO cairo-xlib)
-elseif(UNIX AND APPLE)
-    list(APPEND REQ_PKGCONFIG CAIRO cairo-quartz)
-endif(WIN32)
-
+# media codecs
 list(APPEND REQ_PACKAGES FFMPEG )
 
-list(APPEND REQ_PACKAGES OSCPACK )
 
-if( OSX )
+if(WIN32)
+
+    # pthread emulation library
+    list(APPEND REQ_PACKAGES PthreadVC)
+
+    # cairo backend
+    list(APPEND REQ_PKGCONFIG CAIRO cairo-win32)
+    
+elseif(LINUX)
+
+    # cairo backend
+    list(APPEND REQ_PKGCONFIG CAIRO cairo-xlib)
+    
+elseif(OSX)
+
+    # cairo backend
+    list(APPEND REQ_PKGCONFIG CAIRO cairo-quartz)
+
+    # cocoa ui framework
     find_library( COCOA_LIBRARIES NAMES Cocoa)
     mark_as_advanced( COCOA_LIBRARIES )
-endif( OSX )
+    
+endif(WIN32)
 
