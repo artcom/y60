71329bb1150d19014c78c66be01531d10b810ca4
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-17 21:09:58 +0000

    (CMake) Removed obsolete stub AcRevision.cmake.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21519 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/AcRevision.cmake
+++
@@ -1,8 +0,0 @@
-
-function(ac_generate_revision PROJECT DIRECTORY FILES)
-    add_custom_command(
-        OUTPUT ${ACMAKE_
-        COMMAND svnversion ${DIRECTORY}
-    )
-endfunction(ac_generate_revision)
-
