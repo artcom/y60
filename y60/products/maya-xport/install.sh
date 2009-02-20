43b15bc5d6cc47b3fa6023c1d4c95e27dcaa459c
katjae <katjae@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-20 17:03:55 +0000

    install-script: maya-exporter for windows (now installs into user-specific folder, not tested)
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21673 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/products/maya-xport/install.sh
+++ y60/products/maya-xport/install.sh
@@ -42,20 +42,29 @@ Linux*)
     ;;
 CYGWIN*)
 
-    mkdir -p $MAYA_SDK/plug-ins
-    mkdir -p $MAYA_SDK/scripts/others
-    # XXX is it ok that maya is located in lib while the others are in bin?
-    cp -vf ${BINARY}/lib/Release/maya-xport.dll "$MAYA_SDK/bin/plug-ins/mayaY60export.mll"
-    cp -vf ${BINARY}/bin/Release/aslbase.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/asldom.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/aslmath.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/aslzip.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/y60base.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/y60image.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/y60scene.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/y60video.dll "$MAYA_SDK/bin"
-    cp -vf ${BINARY}/bin/Release/paintlib.dll "$MAYA_SDK/bin"
-    cp -vf ${SOURCE}/*.mel "$MAYA_SDK/scripts/others"
+# install in $MAYA_SDK-DIR
+#   mkdir -p $MAYA_SDK/plug-ins
+#   mkdir -p $MAYA_SDK/scripts/others
+#   # XXX is it ok that maya is located in lib while the others are in bin?
+#   cp -vf ${BINARY}/lib/Release/maya-xport.dll "$MAYA_SDK/bin/plug-ins/mayaY60export.mll"
+#   cp -vf ${BINARY}/bin/Release/aslbase.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/asldom.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/aslmath.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/aslzip.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/y60base.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/y60image.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/y60scene.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/y60video.dll "$MAYA_SDK/bin"
+#   cp -vf ${BINARY}/bin/Release/paintlib.dll "$MAYA_SDK/bin"
+#   cp -vf ${SOURCE}/*.mel "$MAYA_SDK/scripts/others"
+
+    # ToDo: not tested yet (due to broken tree)
+    # install in $home dir (does only work if the dll's are in the path)
+    DESTINATION=$USERPROFILE/"Eigene Dateien"/maya/${MAYA_VERSION}
+    mkdir -p "${DESTINATION}/plug-ins"
+    cp -vf ${BINARY}/lib/Release/maya-xport.dll "${DESTINATION}/plug-ins/mayaY60export.mll"
+    mkdir -p "${DESTINATION}/scripts"
+    cp -vf ${SOURCE}/*.mel "${DESTINATION}/scripts"
     ;;
 esac
 
