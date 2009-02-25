a9be0acd323066b6b9bb78b05b99f725e21680e2
hendrik <hendrik@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-25 09:11:14 +0000

    corrected warning pragma for Win32
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21812 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/glrender/ShaderTypes.h
+++ y60/glrender/ShaderTypes.h
@@ -13,13 +13,13 @@
 // stay consistent.
 
 #if (CG_VERSION_NUM >= 2000)
-    #define AC_NV_CG_20
+#    define AC_NV_CG_20
 #else
-#ifdef WIN32
-#    pragma warn "CG Version < 2, compiling without gp4 support"
-#else
-#    warning "CG Version < 2, compiling without gp4 support" 
-#endif
+#    ifdef WIN32
+#        pragma warning("CG Version < 2, compiling without gp4 support")
+#    else
+#        warning "CG Version < 2, compiling without gp4 support" 
+#    endif
 #endif
 
 namespace y60 {
@@ -27,10 +27,10 @@ namespace y60 {
     enum MaterialType { CG_MATERIAL, FIXED_FUNCTION_MATERIAL };
     enum ShaderType { NO_SHADER_TYPE, FRAGMENT_SHADER, VERTEX_SHADER };
     enum ShaderProfile {NO_PROFILE, GLSLV, GLSLF, ARBVP1, ARBFP1, VP20, FP20, VP30, FP30, VP40, FP40
-#ifdef AC_NV_CG_20
+#   ifdef AC_NV_CG_20
     ,
     GLSLC, GP4FP, GP4VP, GP4GP
-#endif
+#   endif
     };
     
     const char * const ShaderProfileStrings[] = {
@@ -45,12 +45,12 @@ namespace y60 {
         "fp30",
         "vp40",
         "fp40",
-#ifdef AC_NV_CG_20
+#       ifdef AC_NV_CG_20
         "glslc",
         "gp4fp",
         "gp4vp",
         "gp4gp",
-#endif
+#       endif
         0
     };
 
