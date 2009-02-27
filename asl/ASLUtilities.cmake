0ffed643324a5689ab3e02317c6f85489c25bda0
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-02-27 19:00:34 +0000

    (PRO60) Switched to generated cpp file for Y60.xsd. Added tooling for this to ASL.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@21938 f3027e6b-e3f5-0310-a8e1-edde99107618

---
+++ asl/ASLUtilities.cmake
@@ -0,0 +1,30 @@
+
+macro(asl_add_schema XSD)
+    parse_arguments(
+        THIS_XSD
+        "CXX_FILE;CXX_NAMESPACE;CXX_VARNAME;CXX_INCLUDE"
+        ""
+        ${ARGN}
+    )
+
+    set(THIS_XSD_FILE ${XSD})
+
+    if(THIS_XSD_CXX_FILE)
+        get_target_property(
+            TEXT2SRC asl-text2src LOCATION
+        )
+        add_custom_command(
+            OUTPUT  ${THIS_XSD_CXX_FILE}
+            DEPENDS ${THIS_XSD_FILE}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            COMMAND 
+                ${TEXT2SRC}
+                --inputfile  "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_FILE}"
+                --outputfile "${CMAKE_CURRENT_BINARY_DIR}/${THIS_XSD_CXX_FILE}"
+                --include    "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_CXX_INCLUDE}"
+                --namespace  ${THIS_XSD_CXX_NAMESPACE}
+                --varname    ${THIS_XSD_CXX_VARNAME}
+        )
+    endif(THIS_XSD_CXX_FILE)
+
+endmacro(asl_add_schema)
