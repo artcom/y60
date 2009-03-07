41763e47da7414c2e17a638ae3304862e3229be2
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-03-07 01:23:51 +0000

    (CMake) Cleaned up VariableUtils and added a prepend_global macro.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@22139 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/AcVariableUtils.cmake
+++ acmake/modules/AcVariableUtils.cmake
@@ -15,6 +15,16 @@
 # __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
 #
 
+### SCOPED PROPERTIES
+
+macro(clear_locals LOCAL_PREFIX)
+    foreach(SUFFIX ${ARGN})
+        set(${LOCAL_PREFIX}_${SUFFIX})
+    endforeach(SUFFIX)
+endmacro(clear_locals LOCAL_PREFIX SUFFIXES)
+
+### GLOBAL PROPERTIES
+
 macro(set_global NAME)
     set_property(GLOBAL PROPERTY ${NAME} ${ARGN})
 endmacro(set_global NAME)
@@ -30,15 +40,16 @@ macro(append_global_unique NAME)
     set_global(${NAME} ${_TMP})
 endmacro(append_global_unique NAME)
 
+macro(prepend_global NAME)
+    get_global(${NAME} _V)
+    list(INSERT _V 0 ${ARGN})
+    set_global(${NAME} ${_V})
+endmacro(prepend_global NAME)
+
 macro(get_global NAME INTO)
     set(${INTO})
     get_property(${INTO} GLOBAL PROPERTY ${NAME})
 endmacro(get_global NAME INTO)
-
-# XXX: get rid of this
-macro(print_global NAME)
-    get_global(${NAME} MY_VALUE)
-endmacro(print_global NAME)
 
 macro(get_globals GLOBAL_PREFIX LOCAL_PREFIX)
     foreach(SUFFIX ${ARGN})
@@ -51,10 +62,3 @@ macro(set_globals LOCAL_PREFIX GLOBAL_PREFIX)
         set_global(${GLOBAL_PREFIX}_${SUFFIX} ${${LOCAL_PREFIX}_${SUFFIX}})
     endforeach(SUFFIX)
 endmacro(set_globals LOCAL_PREFIX GLOBAL_PREFIX SUFFIXES)
-
-macro(clear_locals LOCAL_PREFIX)
-    foreach(SUFFIX ${ARGN})
-        set(${LOCAL_PREFIX}_${SUFFIX})
-    endforeach(SUFFIX)
-endmacro(clear_locals LOCAL_PREFIX SUFFIXES)
-
