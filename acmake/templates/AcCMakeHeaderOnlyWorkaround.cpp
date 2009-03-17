1aa73a29698567ea84d1d1e889ff25581878b109
david <david@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-03-17 13:29:35 +0000

    (CMake) hide declspec on gcc platforms
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@22375 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/templates/AcCMakeHeaderOnlyWorkaround.cpp
+++ acmake/templates/AcCMakeHeaderOnlyWorkaround.cpp
@@ -1,1 +1,6 @@
-namespace { __declspec( dllexport ) int cmake_header_only_workaround; }
+#ifdef _WIN32
+#   define ACMAKE_CMAKE_HEADER_ONLY_WORKAROUND_DECL __declspec( dllexport )
+#else
+#   define ACMAKE_CMAKE_HEADER_ONLY_WORKAROUND_DECL
+#endif
+namespace { ACMAKE_CMAKE_HEADER_ONLY_WORKAROUND_DECL int cmake_header_only_workaround; }
