11d7d6039e553d8eb41b1ec69f40ee3fcb836d37
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-04-27 09:52:01 +0000

    (ACMake) Improved handling of ffmpeg dependency on linux and osx.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@23387 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/FindFFMPEG.cmake
+++ acmake/modules/FindFFMPEG.cmake
@@ -10,22 +10,27 @@ if(WIN32)
             ${SUBLIB_UPPER}_INCLUDE_DIRS
         )
     endforeach(SUBLIB ${FFMPEG_SUBLIBS})
+
+    set(FFMPEG_ALL_FOUND YES)
 else(WIN32)
     # XXX: required handling is broken here
 
-    pkg_search_module(AVCODEC REQUIRED libavcodec)
+    pkg_search_module(AVCODEC REQUIRED  libavcodec)
     pkg_search_module(AVFORMAT REQUIRED libavformat)
-    pkg_search_module(SWSCALE libswscale)
+    pkg_search_module(SWSCALE           libswscale)
 
     # contains av_free etc and is needed on Mac OS X. I think this is because of
     # incorrect linkage in the ffmpeg build. avformat etc should link avutil instead
     # of leaving the symbols undefined until the libs are linked (I think they use 
     # -undefined dynamic_lookup or something).
     # [DS]
-    pkg_search_module(AVUTIL REQUIRED libavutil ) 
+    pkg_search_module(AVUTIL REQUIRED libavutil) 
 
     pkg_search_module(AVFILTER libavfilter) # optional because it does not exist on linux
 
+    if(AVCODEC_FOUND AND AVFORMAT_FOUND AND AVUTIL_FOUND)
+        set(FFMPEG_ALL_FOUND YES)
+    endif(AVCODEC_FOUND AND AVFORMAT_FOUND AND AVUTIL_FOUND)
 endif(WIN32)
 
 set(FFMPEG_LIBRARIES ${AVCODEC_LIBRARIES} ${AVFORMAT_LIBRARIES} ${AVFILTER_LIBRARIES} ${SWSCALE_LIBRARIES} ${AVUTIL_LIBRARIES})
@@ -35,5 +40,6 @@ mark_as_advanced(FFMPEG_LIBRARIES FFMPEG_LIBRARY_DIRS FFMPEG_INCLUDE_DIRS)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-    FFMPEG DEFAULT_MSG FFMPEG_LIBRARIES
+    FFMPEG DEFAULT_MSG FFMPEG_LIBRARIES FFMPEG_ALL_FOUND
 )
+
