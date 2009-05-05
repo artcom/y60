3e707eecc63ed12a6fa429f581a755298fcd7024
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-05-05 10:52:29 +0000

    (Y60) Re-enabled launcher generation. Bug was a stupid typo.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@23530 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/Y60Utilities.cmake
+++ y60/Y60Utilities.cmake
@@ -134,25 +134,25 @@ macro(y60_add_launcher NAME)
 
     if(UNIX)
         # generate launcher shell script for build tree runs
-#        configure_file(
-#            ${Y60_TEMPLATE_DIR}/Y60BuildLauncher.sh.in
-#            ${CMAKE_CURRENT_BINARY_DIR}/${COMMAND_NAME}
-#            @ONLY
-#        )
+        configure_file(
+            ${Y60_TEMPLATE_DIR}/Y60BuildLauncher.sh.in
+            ${CMAKE_CURRENT_BINARY_DIR}/${THIS_LAUNCHER_COMMAND_NAME}
+            @ONLY
+        )
         # generate launcher shell script for installed tree runs
-#        configure_file(
-#            ${Y60_TEMPLATE_DIR}/Y60InstallLauncher.sh.in
-#            ${CMAKE_CURRENT_BINARY_DIR}/${ACMAKE_BINARY_SUBDIR}/${THIS_LAUNCHER_COMMAND_NAME}
-#            @ONLY
-#        )
-#        install(
-#            FILES ${CMAKE_CURRENT_BINARY_DIR}/${ACMAKE_BINARY_SUBDIR}/${THIS_LAUNCHER_COMMAND_NAME}
-#            COMPONENT ${APPLICATION}
-#            DESTINATION bin/
-#            PERMISSIONS OWNER_EXECUTE GROUP_EXECUTE WORLD_EXECUTE
-#                        OWNER_READ    GROUP_READ    WORLD_READ
-#                        OWNER_WRITE
-#        )
+        configure_file(
+            ${Y60_TEMPLATE_DIR}/Y60InstallLauncher.sh.in
+            ${CMAKE_CURRENT_BINARY_DIR}/${ACMAKE_BINARY_SUBDIR}/${THIS_LAUNCHER_COMMAND_NAME}
+            @ONLY
+        )
+        install(
+            FILES ${CMAKE_CURRENT_BINARY_DIR}/${ACMAKE_BINARY_SUBDIR}/${THIS_LAUNCHER_COMMAND_NAME}
+            COMPONENT ${APPLICATION}
+            DESTINATION bin/
+            PERMISSIONS OWNER_EXECUTE GROUP_EXECUTE WORLD_EXECUTE
+                        OWNER_READ    GROUP_READ    WORLD_READ
+                        OWNER_WRITE
+        )
     endif(UNIX)
 
     if(LINUX)
