75c0344cb107c70c31b32bf059294f83444b089a
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-06-25 15:07:38 +0000

    (Paintlib) Fixed a (spurious) warning.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@24484 f3027e6b-e3f5-0310-a8e1-edde99107618

--- netsrc/paintlib/src/plpngenc.cpp
+++ netsrc/paintlib/src/plpngenc.cpp
@@ -242,7 +242,7 @@ void PLPNGEncoder::DoEncode (PLBmpBase * pBmp, PLDataSink* pDataSnk)
    * currently be PNG_COMPRESSION_TYPE_BASE and PNG_FILTER_TYPE_BASE. REQUIRED
    */
 
-  int bit_depth, color_type;
+  int bit_depth = 0, color_type = 0;
 
   switch(pBmp->GetBitsPerPixel())
   {
