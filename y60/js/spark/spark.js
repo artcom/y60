a765339895ebf113b90424179ec0b7335881cd6e
claudiaw <claudiaw@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-07-01 12:33:06 +0000

    (SPARK) Added newtext to load list.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@24518 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/js/spark/spark.js
+++ y60/js/spark/spark.js
@@ -118,4 +118,5 @@ if (!("disableSparkWidgets" in this)) {
     use("spark/widget/Image.js");
     use("spark/widget/Text.js");
     use("spark/widget/Movie.js");
+    use("spark/widget/NewText.js");
 }
