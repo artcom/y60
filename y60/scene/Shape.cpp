e0feb5f6acaa371ea1e809f5ac4248baed960ef0
valentin <valentin@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-07-03 15:10:00 +0000

    fix for testSceneLeakage, strange thing, probably a ms compilerbug
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@24560 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/scene/Shape.cpp
+++ y60/scene/Shape.cpp
@@ -235,7 +235,7 @@ namespace y60 {
             }
         }
         throw ShapeException(std::string("Could not find vertex data '")+theDataName+"' in shape '"+get<IdTag>()+"'", "Scene::getListData()");
-        //return NodePtr(0);
+        return NodePtr(0); // XXX, breaks testSceneLeakage test but cannot be compiled with boost smartptr (vs)
     }
 
 #if 0 
