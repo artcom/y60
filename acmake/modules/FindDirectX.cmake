5ac4e7a0efdb24cd278702fbc255395caf88200f
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-10-29 12:26:22 +0000

    (AcMake) Extended FindDirectX to use DXSDK_DIR.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@25970 f3027e6b-e3f5-0310-a8e1-edde99107618

--- acmake/modules/FindDirectX.cmake
+++ acmake/modules/FindDirectX.cmake
@@ -1,5 +1,11 @@
 if( WIN32 )
 
+    set(DIRECTX_SDK_DIR $ENV{DXSDK_DIR})
+    if(DIRECTX_SDK_DIR)
+        list(APPEND CMAKE_PREFIX_PATH ${DIRECTX_SDK_DIR})
+    endif(DIRECTX_SDK_DIR)
+    mark_as_advanced(DIRECTX_SDK_DIR)
+
     find_path(DIRECTX_INCLUDE_DIR dxsdkver.h)
     mark_as_advanced(DIRECTX_INCLUDE_DIR)
 
@@ -9,7 +15,7 @@ if( WIN32 )
     set(DIRECTX_LIBRARIES)
     foreach(SUBLIB ${DIRECTX_SUBLIBS})
         mark_as_advanced(DIRECTX_SUBLIB_${SUBLIB})
-        find_library(DIRECTX_SUBLIB_${SUBLIB} NAMES ${SUBLIB})
+        find_library(DIRECTX_SUBLIB_${SUBLIB} NAMES ${SUBLIB} PATHS ${DIRECTX_SDK_DIR}/lib/x86)
         if(DIRECTX_SUBLIB_${SUBLIB}-NOTFOUND)
             set(DIRECTX-NOTFOUND TRUE)
             break()
