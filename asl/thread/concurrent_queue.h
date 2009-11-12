fbe8d942e92277b928dd6382ef98d6cb90c66fdd
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-11-12 19:04:04 +0000

    (ASL) Documented concurrent_queue.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@26253 f3027e6b-e3f5-0310-a8e1-edde99107618

--- asl/thread/concurrent_queue.h
+++ asl/thread/concurrent_queue.h
@@ -10,36 +10,78 @@
 namespace asl {
 namespace thread {
 
+/**
+ * @class concurrent_queue
+ * @brief Locked multi-reader multi-writer queue.
+ *
+ * This class models a thread-safe queue that can be accessed
+ * concurrently by multiple readers and multiple writers.
+ *
+ * It is lock-based and locks on all operations.
+ *
+ */
 template<typename Value_Type_>
 class concurrent_queue {
 
 public:
 
+	/**
+	 * Type of queue elements.
+	 */
     typedef Value_Type_ value_type;
     
+    /**
+     * Type of the inner queue.
+     */
     typedef std::queue<value_type> queue_type;
     
+    /**
+     * Container type used by the inner queue.
+     */
     typedef typename queue_type::container_type container_type;
     
+    /**
+     * Type for sizes concerning the queue.
+     */
     typedef typename container_type::size_type       size_type;
+
+    /**
+     * Type for size differences concerning the queue.
+     */
     typedef typename container_type::difference_type difference_type;
     
+    /**
+     * Reference type for elements.
+     */
     typedef typename container_type::reference       reference;
+
+    /**
+     * Const reference type for elements.
+     */
     typedef typename container_type::const_reference const_reference;
 
 private:
 
-    // our inner queue
+    /**
+     * The inner queue containing our elements.
+     */
     queue_type queue_;
 
-    // mutex protecting queue
+    /**
+     * Mutex protecting the inner queue.
+     */
     mutable boost::mutex mutex_;
 
-    // condition variable signalling pushes
+    /**
+     * Condition variable signaling pushes to the queue.
+     */
     boost::condition_variable condition_variable_;
 
 public:
 
+    /**
+     * Determine if the queue is empty.
+     */
     bool empty() const
     {
         boost::mutex::scoped_lock lock(mutex_);
@@ -47,6 +89,9 @@ public:
         return queue_.empty();
     }
     
+    /**
+     * Determine the number of elements on the queue.
+     */
     size_type size() const
     {
     	boost::mutex::scoped_lock lock(mutex_);
@@ -54,6 +99,9 @@ public:
     	return queue_.size();
     }
 	
+    /**
+     * Push an element onto the queue.
+     */
     void push(const_reference data)
     {
         boost::mutex::scoped_lock lock(mutex_);
@@ -65,6 +113,9 @@ public:
         condition_variable_.notify_one();
     }
 
+    /**
+     * Remove all elements from the queue atomically.
+     */
     void flush()
     {
     	boost::mutex::scoped_lock lock(mutex_);
@@ -74,6 +125,9 @@ public:
     	}
     }
 
+    /**
+     * Try to pop an element off the queue without waiting.
+     */
     bool try_pop(reference popped_value)
     {
         boost::mutex::scoped_lock lock(mutex_);
@@ -89,6 +143,9 @@ public:
         return true;
     }
 
+    /**
+     * Pop an element off the queue, waiting for one if the queue is empty.
+     */
     void wait_and_pop(reference popped_value)
     {
         boost::mutex::scoped_lock lock(mutex_);
@@ -102,10 +159,18 @@ public:
         queue_.pop();
     }
 
+    /**
+     * Pop an element off the queue, waiting for at most
+     * the specified duration if the queue is empty.
+     */
     template<typename Duration_Type_>
-    bool timed_wait_and_pop(reference popped_value, Duration_Type_ wait_duration)
+    bool timed_wait_and_pop(
+    		reference popped_value,
+    		Duration_Type_ wait_duration
+    		)
     {
-        boost::system_time const timeout = boost::get_system_time() + wait_duration;
+        boost::system_time const timeout =
+        		boost::get_system_time() + wait_duration;
 
         boost::mutex::scoped_lock lock(mutex_);
 
