7f9849d0aee5b5529dfed4afb54b42656fa5e4d9
ingo <ingo@f3027e6b-e3f5-0310-a8e1-edde99107618>
2009-12-07 17:51:58 +0000

    (SPARK) Incresed default camera distance.
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@26588 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/js/spark/camera.js
+++ y60/js/spark/camera.js
@@ -63,6 +63,6 @@ spark.setupCameraOrtho = function(theCamera, theWidth, theHeight) {
     theCamera.frustum.type = ProjectionType.orthonormal;
     theCamera.position.x = theWidth/2;
     theCamera.position.y = theHeight/2;
-    theCamera.position.z = 500;
+    theCamera.position.z = 2500;
     theCamera.orientation = Quaternionf.createFromEuler(new Vector3f(0,0,0));        
 }
