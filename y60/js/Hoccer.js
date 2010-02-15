823599212a6e81084402b822f04fdf206c7c58c2
katjae <katjae@f3027e6b-e3f5-0310-a8e1-edde99107618>
2010-02-15 17:31:33 +0000

    (js/hoccer) first experiments with requestManager
    git-svn-id: https://svn.intern.artcom.de/pro60/trunk/src@27319 f3027e6b-e3f5-0310-a8e1-edde99107618

--- y60/js/Hoccer.js
+++ y60/js/Hoccer.js
@@ -1,17 +1,48 @@
+use("HttpClient.js");
+
 var Hoccer = {};
+
+
 
 Hoccer.station = function() {
     var that = {};
+    var requestManager = new RequestManager();
 
-    that.init = function(theLongitude, theLatitude, theAccuracy) {
-        that.longitude = theLongitude;
-        that.latitude = theLatitude;
-        that.accuracy = theAccuracy;
-    }
+    that.userAgent = "Hoccer/0.9dev Y60";
+
+    //default hoccer station at artcom
+    that.longitude = 13.345116;
+    that.latitude = 52.501077;
+    that.accuracy = 80;
+    
+
 
     that.distribute = function(theFile) {
         print("distribute");
-    }
+        
+        var request = new Request("http://www.artcom.de/",  that.userAgent ); 
+        
+        request.onDone  = function() {
+            print("handle response: ", this.responseString, "  code: ", this.responseCode);
+        }
+
+
+        request.onError = function () {
+            Logger.warning( "HTTP Code received: " 
+                            + this.responseCode + " for request '" 
+                            + thePath + "'." ); 
+        };
+
+        request.get(); 
+        requestManager.performRequest(request );
+
+    };
+
+    that.update = function() {
+        requestManager.handleRequests();
+    };
+
+
 
     return that;
 }();
