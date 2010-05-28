6835dfdf3f2a8dfbf1e2a1575cc8beac98d80295
gunnar <gunnar@artcom.de>
2010-05-28 16:41:37 +0200

    fixed penner easing

--- y60/js/Easing.js
+++ y60/js/Easing.js
@@ -199,21 +199,21 @@ Easing.easeInOutCirc = function (t, b, c, d) {
 
 Easing.easeInElastic = function (t, b, c, d, a, p) {
 	if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-	if (a < Math.abs(c)) { a=c; var s=p/4; }
+	if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
 	else var s = p/(2*Math.PI) * Math.asin (c/a);
 	return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
 };
 
 Easing.easeOutElastic = function (t, b, c, d, a, p) {
 	if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-	if (a < Math.abs(c)) { a=c; var s=p/4; }
+	if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
 	else var s = p/(2*Math.PI) * Math.asin (c/a);
-	return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b);
 };
 
 Easing.easeInOutElastic = function (t, b, c, d, a, p) {
 	if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
-	if (a < Math.abs(c)) { a=c; var s=p/4; }
+	if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
 	else var s = p/(2*Math.PI) * Math.asin (c/a);
 	if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
 	return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
