9c16a45d5faf4624307fb7f60313b640ee664c18
Valentin Schunack <valentin.schunack@artcom.de>
2010-05-31 17:29:38 +0200

    little more verbose on last workaround checkin

--- y60/components/video/DShowCapture/DShowGraph.h
+++ y60/components/video/DShowCapture/DShowGraph.h
@@ -72,6 +72,7 @@
 #define __IDxtAlphaSetter_INTERFACE_DEFINED__
 #define __IDxtJpeg_INTERFACE_DEFINED__
 #define __IDxtKey_INTERFACE_DEFINED__
+// end-workaround
 #include <qedit.h>
 
 #include <string>
