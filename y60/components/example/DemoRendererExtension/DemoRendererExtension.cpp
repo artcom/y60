52f72f8e0819e42be49ec9139deda1d907a0cf69
Andreas Marr <andreasm@artcom.de>
2010-06-02 13:40:43 +0200

    updated DemoRendererExtension. Omitted settings and included example javascript function (setBodyId). Must be executed with cube.x60 as argument

--- y60/components/example/DemoRendererExtension/DemoRendererExtension.cpp
+++ y60/components/example/DemoRendererExtension/DemoRendererExtension.cpp
@@ -81,6 +81,8 @@ using namespace jslib;
 using namespace dom;
 using namespace y60;
 
+#define ASSERT(x) if (!(x)) {cerr << "Assertion fialed: " << #x << " " << __FILE__ << " " <<__LINE__ << endl; exit(1);}
+
 DEFINE_EXCEPTION(CubeNotFound, asl::Exception)
 
     class DemoRendererExtension :
@@ -91,28 +93,29 @@ DEFINE_EXCEPTION(CubeNotFound, asl::Exception)
     public:
         DemoRendererExtension(asl::DLHandle theDLHandle);
 
+        JSFunctionSpec * Functions();
+
         void onStartup(jslib::AbstractRenderWindow * theWindow);
-
+        
         bool onSceneLoaded(jslib::AbstractRenderWindow * theWindow);
-
+        
         void handle(AbstractRenderWindow * theWindow, y60::EventPtr theEvent);
         void onFrame(AbstractRenderWindow * theWindow , double t);
-
+        
         void onPreRender(AbstractRenderWindow * theRenderer);
         void onPostRender(AbstractRenderWindow * theRenderer);
-
-		const char * ClassName() {
-		    static const char * myClassName = "DemoRendererExtension";
-		    return myClassName;
-		}
-
-		void onUpdateSettings(dom::NodePtr theSettings) {
-		    AC_PRINT << "New Settings: " << *theSettings << endl;
-		}
+        
+        void setBodyId(const std::string & theBodyId);
+        
+        const char * ClassName() {
+            static const char * myClassName = "DemoRendererExtension";
+            return myClassName;
+        }
 
     private:
         NodePtr _myCube;
         y60::BodyPtr _myCubeBody;
+        y60::ScenePtr _myScene;
     };
 
 DemoRendererExtension :: DemoRendererExtension(DLHandle theDLHandle) :
@@ -121,23 +124,31 @@ DemoRendererExtension :: DemoRendererExtension(DLHandle theDLHandle) :
 {
 }
 
+// Native version for a function that is later on wrapped into Javascript
+void
+DemoRendererExtension :: setBodyId(const std::string & theBodyId) {
+    if (_myScene) {
+        NodePtr mySceneDom = _myScene->getSceneDom();
+        _myCube = mySceneDom->getElementById(theBodyId);
+        if (!_myCube) {
+            throw CubeNotFound("Body '" + theBodyId + "' not found.", PLUS_FILE_LINE);
+        }
+        _myCubeBody = _myCube->getFacade<Body>();
+        AC_DEBUG << "Body found";
+    } else {
+        AC_WARNING << "Scene not found";
+    }
+}
+
 void
 DemoRendererExtension :: onStartup(jslib::AbstractRenderWindow * theWindow) {
 }
 
 bool
 DemoRendererExtension :: onSceneLoaded(jslib::AbstractRenderWindow * theWindow) {
-    y60::ScenePtr myScene;
     AC_DEBUG << "onSceneLoaded...";
-    myScene = theWindow->getRenderer()->getCurrentScene();
-    if (myScene) {
-        NodePtr mySceneDom = myScene->getSceneDom();
-        _myCube = mySceneDom->getElementById("b0");
-        if (!_myCube) {
-            throw CubeNotFound("Body 'b0' not found.", PLUS_FILE_LINE);
-        }
-        _myCubeBody = _myCube->getFacade<Body>();
-    } else {
+    _myScene = theWindow->getRenderer()->getCurrentScene();
+    if (!_myScene) {
         AC_ERROR << "onSceneLoaded: no scene found." << endl;
     }
     AC_DEBUG << "...onSceneLoaded";
@@ -151,12 +162,16 @@ DemoRendererExtension :: handle(AbstractRenderWindow * theWindow, y60::EventPtr 
 
 void
 DemoRendererExtension :: onFrame(AbstractRenderWindow * theWindow , double t) {
-    float myPeriod = 2.0f;
-
-    AC_DEBUG << "onFrame...";
-    Quaternionf myOrientation(Vector3f(0, 1, 0), (float)((t / myPeriod) * asl::PI));
-    _myCubeBody->set<OrientationTag>(myOrientation);
-    AC_DEBUG << "...onFrame";
+    if (_myCubeBody) {
+        float myPeriod = 2.0f;
+        
+        AC_DEBUG << "onFrame...";
+        Quaternionf myOrientation(Vector3f(0, 1, 0), (float)((t / myPeriod) * asl::PI));
+        _myCubeBody->set<OrientationTag>(myOrientation);
+        AC_DEBUG << "...onFrame";
+    } else {
+        AC_DEBUG << "bodyId is not (yet) yet. doing nothing";
+    }
 }
 
 void
@@ -165,6 +180,40 @@ DemoRendererExtension :: onPreRender(AbstractRenderWindow * theRenderer) {
 
 void
 DemoRendererExtension :: onPostRender(AbstractRenderWindow * theRenderer) {
+}
+
+////////////////////////////////////
+// Spidermonkey Javascript Bindings
+////////////////////////////////////
+
+// Wrapper for setBodyId (note the upper- or lower- cased first character)
+static JSBool
+SetBodyId(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    DOC_BEGIN("Set the body id of the body to be affected. Only works after scene setup.");
+    DOC_END;
+    asl::Ptr<DemoRendererExtension> myNative = getNativeAs<DemoRendererExtension>(cx, obj);
+    ASSERT(myNative);
+    ensureParamCount(argc, 1);
+    
+    std::string newBodyId = "";
+    if ( ! convertFrom(cx, argv[0], newBodyId)) {
+        JS_ReportError(cx, "argument must be a string");
+        return JS_FALSE;
+    }
+    
+    myNative->setBodyId(newBodyId);
+    
+    return JS_TRUE;
+}
+
+// Spidermonkey functions map
+JSFunctionSpec *
+DemoRendererExtension::Functions() {
+    static JSFunctionSpec myFunctions[] = {
+        {"setBodyId", SetBodyId, 1},
+        {0}
+    };
+    return myFunctions;
 }
 
 extern "C"
--- y60/components/example/DemoRendererExtension/DemoRendererExtension.js
+++ y60/components/example/DemoRendererExtension/DemoRendererExtension.js
@@ -69,7 +69,6 @@ if (__main__ == undefined) var __main__ = "DemoRendererExtension";
 use("SceneViewer.js");
 plug("DemoRendererExtension");
 
-
 function DemoRendererExtensionApp(theArguments) {
     this.Constructor(this, theArguments);
 }
@@ -78,9 +77,7 @@ DemoRendererExtensionApp.prototype.Constructor = function(self, theArguments) {
     var _myTextOverlay = null;
 
     //////////////////////////////////////////////////////////////////////
-    //
     // Constructor
-    //
     //////////////////////////////////////////////////////////////////////
 
     SceneViewer.prototype.Constructor(self, theArguments);
@@ -88,9 +85,7 @@ DemoRendererExtensionApp.prototype.Constructor = function(self, theArguments) {
     var _myDemoRenderExtension = new DemoRendererExtension();
 
     //////////////////////////////////////////////////////////////////////
-    //
     // public members
-    //
     //////////////////////////////////////////////////////////////////////
 
     // setup
@@ -98,61 +93,7 @@ DemoRendererExtensionApp.prototype.Constructor = function(self, theArguments) {
     self.setup = function(theWidth, theHeight, theTitle) {
         Base.setup(theWidth, theHeight, false, theTitle);
         window.addExtension(_myDemoRenderExtension);
-        self.registerSettingsListener(_myDemoRenderExtension, "DemoBehaviour");
-        createTextOverlay();
-    }
-
-    Base.onKey = self.onKey;
-    self.onKey = function(theKey, theState, theX, theY, theShiftFlag, theCtrlFlag, theAltFlag) {
-        Base.onKey(theKey, theState, theX, theY, theShiftFlag, theCtrlFlag, theAltFlag);
-    }
-
-    Base.onFrame = self.onFrame;
-    self.onFrame = function(theTime) {
-        Base.onFrame(theTime);
-    }
-
-    Base.onMouseMotion = self.onMouseMotion;
-    self.onMouseMotion = function(theX, theY) {
-        if (Base.onMouseMotion) {
-            Base.onMouseMotion(theX, theY);
-        }
-    }
-
-    Base.onMouseButton = self.onMouseButton;
-    self.onMouseButton = function(theButton, theState, theX, theY) {
-        if (Base.onMouseButton) {
-            Base.onMouseButton(theButton, theState, theX, theY);
-        }
-    }
-
-    Base.onMouseWheel = self.onMouseWheel;
-    self.onMouseWheel = function(theDeltaX, theDeltaY) {
-        if (Base.onMouseWheel) {
-            Base.onMouseWheel(theDeltaX, theDeltaY);
-        }
-    }
-
-    ////////////// private members ////////////
-
-    function createTextOverlay() {
-        window.loadTTF("Arial", "${PRO}/src/y60/gl/text/fonts/arial.ttf", 18);
-        window.setTextPadding(10,10,10,10);
-        window.setTextColor([0.2,0.2,1,1]);
-
-        var myTextImage = self.getImageManager().getImageNode("infobox");
-        _myTextOverlay = new ImageOverlay(window.scene, myTextImage);
-        _myTextOverlay.applyMode = "decal";
-        _myTextOverlay.color = new Vector4f(0.8,0.8,1,0.6);
-
-        var myTextSize = window.renderTextAsImage(myTextImage, "Press Ctrl-Q to quit.", "Arial", 300, 0);
-        _myTextOverlay.width  = myTextSize.x;
-        _myTextOverlay.height = myTextSize.y;
-        _myTextOverlay.srcsize.x = myTextSize.x / myTextImage.width;
-        _myTextOverlay.srcsize.y = myTextSize.y / myTextImage.height;
-
-        _myTextOverlay.position = new Vector2f(10, 30);
-        _myTextOverlay.visible = true;
+        _myDemoRenderExtension.setBodyId("b0");
     }
 
 }
@@ -175,5 +116,3 @@ if (__main__ == "DemoRendererExtension") {
         exit(1);
     }
 }
-
-
