501186d992ae27d98ce5d15b6d9d589acf9e51a2
gunnar <gunnar@artcom.de>
2010-06-03 13:33:31 +0200

    fixed xsd.cpp generation for linux&mac

--- asl/ASLUtilities.cmake
+++ asl/ASLUtilities.cmake
@@ -13,18 +13,33 @@ macro(asl_add_schema XSD)
         get_target_property(
             TEXT2SRC text2src LOCATION
         )
-        add_custom_command(
-            OUTPUT  ${THIS_XSD_CXX_FILE}
-            DEPENDS ${THIS_XSD_FILE} text2src
-            WORKING_DIRECTORY ${PRO60_DEPS_ROOT_DIR}/bin
-            COMMAND 
-                ${TEXT2SRC}
-                --inputfile  "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_FILE}"
-                --outputfile "${CMAKE_CURRENT_BINARY_DIR}/${THIS_XSD_CXX_FILE}"
-                --include    "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_CXX_INCLUDE}"
-                --namespace  ${THIS_XSD_CXX_NAMESPACE}
-                --varname    ${THIS_XSD_CXX_VARNAME}
-        )
+        if(WIN32)
+            add_custom_command(
+                OUTPUT  ${THIS_XSD_CXX_FILE}
+                DEPENDS ${THIS_XSD_FILE} text2src
+                WORKING_DIRECTORY ${PRO60_DEPS_ROOT_DIR}/bin
+                COMMAND 
+                    ${TEXT2SRC}
+                    --inputfile  "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_FILE}"
+                    --outputfile "${CMAKE_CURRENT_BINARY_DIR}/${THIS_XSD_CXX_FILE}"
+                    --include    "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_CXX_INCLUDE}"
+                    --namespace  ${THIS_XSD_CXX_NAMESPACE}
+                    --varname    ${THIS_XSD_CXX_VARNAME}
+            )
+        else(WIN32)
+            add_custom_command(
+                OUTPUT  ${THIS_XSD_CXX_FILE}
+                DEPENDS ${THIS_XSD_FILE} text2src
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMAND 
+                    ${TEXT2SRC}
+                    --inputfile  "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_FILE}"
+                    --outputfile "${CMAKE_CURRENT_BINARY_DIR}/${THIS_XSD_CXX_FILE}"
+                    --include    "${CMAKE_CURRENT_SOURCE_DIR}/${THIS_XSD_CXX_INCLUDE}"
+                    --namespace  ${THIS_XSD_CXX_NAMESPACE}
+                    --varname    ${THIS_XSD_CXX_VARNAME}
+            )
+        endif(WIN32)
     endif(THIS_XSD_CXX_FILE)
 
 endmacro(asl_add_schema)
