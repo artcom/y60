d0cbf93e364d1f3c27145c6595c1a77d0f39ef7c
Frauke Fritz <frauke@artcom.de>
2010-06-04 13:40:28 +0200

    (spark) Canvas widget can now also handle 3d models via sceneFile Attribute, not fully implemented yet, texture and multiple loading still to come

--- y60/js/spark/widget/Canvas.js
+++ y60/js/spark/widget/Canvas.js
@@ -10,7 +10,7 @@ spark.Canvas.Constructor = function(Protected) {
     var Public = this;
     var Base = {};
 
-    this.Inherit(spark.Stage);
+    //this.Inherit(spark.Stage);
     this.Inherit(spark.ResizableRectangle);
 
     var _myRenderArea = null;
@@ -31,36 +31,64 @@ spark.Canvas.Constructor = function(Protected) {
     										myWidth, myHeight, "BGRA");
     	myImage.name = Public.name + "_canvasImage";
     	var myTexture = Modelling.createTexture(window.scene, myImage);
-
-        myTexture.wrapmode = "clamp_to_edge";
+      myTexture.wrapmode = "clamp_to_edge";
 
     	var myFlipMatrix = myTexture.matrix;
     	myFlipMatrix.makeScaling(new Vector3f(1,-1,1));
-        myFlipMatrix.translate(new Vector3f(0,1,0));
+      myFlipMatrix.translate(new Vector3f(0,1,0));
 
     	var myMaterial = Modelling.createUnlitTexturedMaterial(window.scene, myTexture);
-        myMaterial.transparent = true;
+      myMaterial.transparent = true;
+      
+      //TODO: check if maybe there are other options than scene merging
+      var mySceneFile = Protected.getString("sceneFile", "");
+      
+      if (mySceneFile) {
+          Logger.info("Loading spark file " + mySceneFile);
+          if (!fileExists(mySceneFile)) {
+              throw Error("spark file '" + theFile + "' does not exist.");
+          }
+          var myDom = new Node();
+          myDom.parseFile(mySceneFile);
+          
+          // get first world, if more world exist we wont be able to handle the second one
+          var myWorldId = myDom.find(".//worlds/world").id;
+          var myCanvasId = myDom.find(".//canvases/canvas").id;
+          
+          mergeScenes(window.scene, myDom);
+          
+          var myCanvas = window.scene.dom.find(".//canvases/canvas[@id='" + myCanvasId + "']");
+          myViewport = myCanvas.find(".//viewport");
+          myCamera   = window.scene.dom.getElementById(myWorldId).find(".//camera");
+          
+          _myWorld = window.scene.dom.getElementById(myWorldId);
+          var myLightManager = new LightManager(window.scene, _myWorld);
+   //     Public.setLightManager(myLightManager);
+          myLightManager.setupDefaultLighting(myCanvas);
+        
+      } else {
+          _myWorld = Node.createElement("world");
+        	window.scene.worlds.appendChild(_myWorld);
+          _myWorld.name = Public.name + "-world";
 
-    	_myWorld = Node.createElement("world");
-    	window.scene.worlds.appendChild(_myWorld);
-        _myWorld.name = Public.name + "-world";
+        	var myCamera = Node.createElement("camera");
+        	_myWorld.appendChild(myCamera);
 
-    	var myCamera = Node.createElement("camera");
-    	_myWorld.appendChild(myCamera);
+        	spark.setupCameraOrtho(myCamera, myWidth, myHeight);
 
-    	spark.setupCameraOrtho(myCamera, myWidth, myHeight);
-
-    	var myCanvas = Node.createElement("canvas");
-    	myCanvas.target = myTexture.id;
-    	window.scene.canvases.appendChild(myCanvas);
-        myCanvas.name = Public.name + "-canvas";
-        myCanvas.backgroundcolor[3] = 0.0;
-
-    	var myViewport = Node.createElement("viewport");
-    	myViewport.camera = myCamera.id;
-    	myCanvas.appendChild(myViewport);
-        myViewport.name = Public.name + "-viewport";
-
+        	var myCanvas = Node.createElement("canvas");
+        	window.scene.canvases.appendChild(myCanvas);
+          myCanvas.name = Public.name + "-canvas";
+          myCanvas.backgroundcolor[3] = 0.0;
+          
+        	var myViewport = Node.createElement("viewport");
+        	myViewport.camera = myCamera.id;
+        	myCanvas.appendChild(myViewport);
+          myViewport.name = Public.name + "-viewport";
+      }
+      
+      myCanvas.target = myTexture.id;
+      
     	_myRenderArea.setSceneAndCanvas(window.scene, myCanvas);
 
     	Public.parent.stage.addEventListener(spark.StageEvent.FRAME, function(theEvent) {
@@ -68,6 +96,25 @@ spark.Canvas.Constructor = function(Protected) {
     	});
 
     	Base.realize(myMaterial);
+    };
+    
+    function mergeScenes(theTargetScene, theModelDom) {
+        //TODO: what if id already exists??
+        //  - preparse scenes, check for equal ids
+        //  - when duplicate ids exist rewrite id and references on id
+        //      - maybe just for viewport, canvas and camera ids because we need them, the rest would be same as already in scene???
+        //  - paths for model resources have to be adjusted, they are relative to sceneFile path
+        var mySceneNode = theModelDom.firstChild;
+        while (mySceneNode.childNodesLength()) {
+            var childNode = mySceneNode.firstChild;
+            var receivingNode = theTargetScene.dom.getNodesByTagName(childNode.nodeName)[0];
+            while (childNode.childNodesLength()) {
+                var childChildNode = childNode.removeChild(childNode.firstChild);
+                receivingNode.appendChild(childChildNode);
+            }
+            mySceneNode.removeChild(mySceneNode.firstChild);
+        }
+        
     };
 
 }
