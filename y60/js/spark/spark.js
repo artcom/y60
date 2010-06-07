8840b1482ec8565f6e5dbee7c72ea2e5b5036a40
gunnar <gunnar@artcom.de>
2010-06-07 13:33:34 +0200

    moved additional spark components to 'add-ons'

--- y60/js/spark/spark.js
+++ y60/js/spark/spark.js
@@ -132,9 +132,9 @@ if (!("disableSparkWidgets" in this)) {
     use("spark/widget/NewText.js");
     use("spark/widget/Canvas.js");
     use("spark/widget/OffscreenCanvas.js");
-    use("spark/widget/RoundedCornerQuad.js");
-    use("spark/widget/RoundedCornerMovie.js");
-    use("spark/widget/RoundedCornerImage.js");
-    use("spark/widget/StretchyImage.js");
-    use("spark/widget/VideoCapture.js");
+    use("spark/add-ons/RoundedCornerQuad.js");
+    use("spark/add-ons/RoundedCornerMovie.js");
+    use("spark/add-ons/RoundedCornerImage.js");
+    use("spark/add-ons/StretchyImage.js");
+    use("spark/add-ons/VideoCapture.js");
 }
