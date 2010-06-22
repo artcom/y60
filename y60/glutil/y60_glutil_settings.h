959dfe5cbf666ecafed7d5b3a6ba50498fa64edd
Sebastian Heymann <sebastianh@limerick.intern.artcom.de>
2010-06-22 11:44:03 +0200

    moved plattform related OpenGL #defines to y60_glutil_settings.h and corrected AC_USE_OSX_CGL related issues and warnings

--- y60/glutil/y60_glutil_settings.h
+++ y60/glutil/y60_glutil_settings.h
@@ -9,4 +9,23 @@
 #   define Y60_GLUTIL_DECL AC_DLL_IMPORT
 #endif
 
+#ifdef OSX
+#   define AC_IGNORE_NV_MEMORY_MANAGEMENT
+#   define AC_USE_OSX_CGL
+#endif
+
+#ifdef _WIN32
+#    define NOMINMAX
+#    ifndef WIN32_LEAN_AND_MEAN
+#       define WIN32_LEAN_AND_MEAN 1
+#    endif
+#    include <windows.h>
+#    undef WIN32_LEAN_AND_MEAN
+#endif
+
+#if defined(LINUX)
+#   define AC_USE_X11
+#endif
+
+
 #endif // Y60_GLUTIL_SETTING_INCLUDED
