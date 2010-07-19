dbf23ef3d4a41539438f0a0bbf7b68916117e785
gunnar <gunnar@artcom.de>
2010-07-19 14:51:30 +0200

    (asl) handle return value

--- asl/base/file_functions.cpp
+++ asl/base/file_functions.cpp
@@ -634,8 +634,12 @@ namespace asl {
         return std::string(_getcwd( NULL, 0 ));
 #else
         char myBuffer[1024];
-        getcwd( myBuffer, 1024);
-        return std::string(myBuffer);
+        if (getcwd( myBuffer, 1024) == myBuffer) {
+            return std::string(myBuffer);
+        } else {
+            throw asl::Exception(std::string("getcwd() failed with error: ") + strerror(errno), PLUS_FILE_LINE);
+            return "";
+        }
 #endif
     }
 
