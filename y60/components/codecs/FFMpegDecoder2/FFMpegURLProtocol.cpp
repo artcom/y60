af723f7a78cedf601e3c09d58fde751c910ec20c
gunnar <gunnar@artcom.de>
2010-07-19 13:17:44 +0200

    (FFMpegDecoder2) fixed last commit

--- y60/components/codecs/FFMpegDecoder2/FFMpegURLProtocol.cpp
+++ y60/components/codecs/FFMpegDecoder2/FFMpegURLProtocol.cpp
@@ -64,24 +64,6 @@
 #include <asl/base/Block.h>
 #include <asl/base/MappedBlock.h>
 
-#ifdef OSX
-    extern "C" {
-#       include <libavformat/avformat.h>
-    }
-#   undef AV_NOPTS_VALUE
-#   define AV_NOPTS_VALUE 0x8000000000000000LL
-#else
-#   if defined(_MSC_VER)
-#       pragma warning(push,1)
-#   endif
-    extern "C" {
-#   include <avformat.h>
-    }
-#   if defined(_MSC_VER)
-#       pragma warning(pop)
-#   endif
-#endif
-
 using namespace asl;
 using namespace std;
 
