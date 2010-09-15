0cb758620fda03017eddb7c6b0125d4607e67234
Martin Skinner <martin@artcom.de>
2010-09-15 15:10:09 +0200

    (y60/js) improved error messages in JSTextureAtlas

--- y60/jslib/JSTextureAtlas.cpp
+++ y60/jslib/JSTextureAtlas.cpp
@@ -197,15 +197,15 @@ JSTextureAtlas::Constructor(JSContext *cx, JSObject *obj, uintN argc, jsval *arg
     DOC_BEGIN("Constructs a TextureAtlas from an atlas definition file");
     DOC_PARAM("theAtlasFile", "Filename of an atlas xml file", DOC_TYPE_STRING);
     DOC_END;
-    if (JSA_GetClass(cx,obj) != Class()) {
-        JS_ReportError(cx,"Constructor for %s  bad object; did you forget a 'new'?",ClassName());
-        return JS_FALSE;
-    }
-    JSTextureAtlas * myNewObject = 0;
-
-    ensureParamCount(argc, 1, 3, PLUS_FILE_LINE);
-
     try {
+        if (JSA_GetClass(cx,obj) != Class()) {
+            JS_ReportError(cx,"Constructor for %s  bad object; did you forget a 'new'?",ClassName());
+            return JS_FALSE;
+        }
+        JSTextureAtlas * myNewObject = 0;
+
+
+        ensureParamCount(argc, 1, 3, PLUS_FILE_LINE);
         if (argc == 0) {
             // can we skip this?
             AC_WARNING << "creating temp TextureAtlas";
@@ -222,7 +222,7 @@ JSTextureAtlas::Constructor(JSContext *cx, JSObject *obj, uintN argc, jsval *arg
             // first argument
             if (argc == 1 && JSVAL_IS_STRING(argv[0]) && convertFrom(cx, argv[0], atlasFilename)) {
                 // load xml & bitmap from files
-                newTextureAtlas = OWNERPTR(new TextureAtlas(Path(atlasFilename, UTF8)));
+                newTextureAtlas = OWNERPTR(new TextureAtlas(Path(atlasFilename, UTF8), JSApp::getPackageManager()));
             } else if (JSVAL_IS_OBJECT(argv[0])) {
                 // generate a new texture atlas
                 TextureAtlas::Subtextures subtextures = createSubtexturesFromJSObject(cx, JSVAL_TO_OBJECT(argv[0]));
