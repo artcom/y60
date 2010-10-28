4d12becd5c412bdb1cf8bc6184bee3c3c3205a4e
katjae <katjae@artcom.de>
2010-10-28 11:34:09 +0200

    (y60) FindOpenCV adaptions for unix

--- acmake/modules/FindOpenCV.cmake
+++ acmake/modules/FindOpenCV.cmake
@@ -1,38 +1,47 @@
-if(WIN32)
 
-    FIND_PATH(OPENCV_INCLUDE_DIR cxcore.h
-        PATH_SUFFIXES opencv
-        PATHS
-        "C:/OpenCV2.1/include")
+FIND_PATH(OPENCV_INCLUDE_DIR cv.h cxcore.h
+    PATH_SUFFIXES opencv
+    PATHS
+    "C:/OpenCV2.1/include"
+    /usr/local/include
+)
 
-    set(OPENCV_INCLUDE_DIRS ${OPENCV_INCLUDE_DIR})
-    mark_as_advanced(OPENCV_DEFINITIONS OPENCV_INCLUDE_DIR OPENCV_INCLUDE_DIRS)
+set(OPENCV_INCLUDE_DIRS ${OPENCV_INCLUDE_DIR})
+mark_as_advanced(OPENCV_DEFINITIONS OPENCV_INCLUDE_DIR OPENCV_INCLUDE_DIRS)
 
+if(WIN32) 
     set(OPENCV_SUBLIBS cv210.lib cvaux210.lib cxcore210.lib cxts210.lib highgui210.lib ml210.lib)
-    mark_as_advanced(OPENCV_SUBLIBS)
-
-    set(OPENCV_LIBRARIES)
-    foreach(SUBLIB ${OPENCV_SUBLIBS})
-        mark_as_advanced(OPENCV_SUBLIB_${SUBLIB})
-        find_library(OPENCV_SUBLIB_${SUBLIB} NAMES ${SUBLIB}  PATH_SUFFIXES lib PATHS "C:/OpenCV2.1")
-        list(APPEND OPENCV_LIBRARIES ${OPENCV_SUBLIB_${SUBLIB}})        
-    endforeach(SUBLIB ${OPENCV_ALL_SUBLIBS})
-        
-
-    set(OPENCV_SUBLIBS_D cv210d.lib cvaux210d.lib cxcore210d.lib cxts210d.lib highgui210d.lib ml210d.lib)
-    mark_as_advanced(OPENCV_SUBLIBS_D)
-
-    set(OPENCV_LIBRARIES_D)
-    foreach(SUBLIB ${OPENCV_SUBLIBS})
-        mark_as_advanced(OPENCV_SUBLIB_${SUBLIB}_D)
-        find_library(OPENCV_SUBLIB_${SUBLIB}_D NAMES ${SUBLIB}  PATH_SUFFIXES lib PATHS "C:/OpenCV2.1")
-        list(APPEND OPENCV_LIBRARIES_D ${OPENCV_SUBLIB_${SUBLIB}})        
-    endforeach(SUBLIB ${OPENCV_ALL_SUBLIBS})
-
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(
-        OPENCV DEFAULT_MSG
-        OPENCV_LIBRARIES OPENCV_LIBRARIES_D OPENCV_INCLUDE_DIR
-    )
-
+else(WIN32)
+    set(OPENCV_SUBLIBS cv cvaux cxcore highgui ml)
 endif(WIN32)
+
+mark_as_advanced(OPENCV_SUBLIBS)
+
+set(OPENCV_LIBRARIES)
+foreach(SUBLIB ${OPENCV_SUBLIBS})
+    mark_as_advanced(OPENCV_SUBLIB_${SUBLIB})
+    find_library(OPENCV_SUBLIB_${SUBLIB} NAMES ${SUBLIB}  PATH_SUFFIXES lib PATHS "C:/OpenCV2.1" /usr/local/lib )
+    list(APPEND OPENCV_LIBRARIES ${OPENCV_SUBLIB_${SUBLIB}})        
+endforeach(SUBLIB ${OPENCV_ALL_SUBLIBS})
+    
+
+if(WIN32)
+    set(OPENCV_SUBLIBS_D cv210d.lib cvaux210d.lib cxcore210d.lib cxts210d.lib highgui210d.lib ml210d.lib)
+else(WIN32)
+    set(OPENCV_SUBLIBS_D cv cvaux cxcore highgui ml)
+endif(WIN32)
+mark_as_advanced(OPENCV_SUBLIBS_D)
+
+set(OPENCV_LIBRARIES_D)
+foreach(SUBLIB ${OPENCV_SUBLIBS})
+    mark_as_advanced(OPENCV_SUBLIB_${SUBLIB}_D)
+    find_library(OPENCV_SUBLIB_${SUBLIB}_D NAMES ${SUBLIB}  PATH_SUFFIXES lib PATHS "C:/OpenCV2.1" /usr/local/lib )
+    list(APPEND OPENCV_LIBRARIES_D ${OPENCV_SUBLIB_${SUBLIB}})        
+endforeach(SUBLIB ${OPENCV_ALL_SUBLIBS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    OPENCV DEFAULT_MSG
+    OPENCV_LIBRARIES OPENCV_LIBRARIES_D OPENCV_INCLUDE_DIR
+)
+
