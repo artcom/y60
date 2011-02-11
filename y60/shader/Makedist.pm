70929e231f386a289759403fe8ff4b8e8c6ebb40
gunnar <gunnar@artcom.de>
2011-02-11 11:35:48 +0100

    (y60/shader) removed makedist file

--- y60/shader/Makedist.pm
+++
@@ -1,9 +0,0 @@
-package Makedist;
-use MakedistTools;
-
-sub runLocalMakedist($) {
-    copyDirectory(".", "$_[0]", (".xml", ".cg"));      
-    copyDirectory("shadertex", "$_[0]/shadertex", (".*"));      
-}
-
-1;
