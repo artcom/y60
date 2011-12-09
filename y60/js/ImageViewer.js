a08501621b0a180816b7367bb7a0b2f02803dd84
gunnar <gunnar@artcom.de>
2011-12-09 12:26:50 +0100

    (y60/js) fixed yuva support for ImageViewer

--- y60/js/ImageViewer.js
+++ y60/js/ImageViewer.js
@@ -685,7 +685,7 @@ ImageViewerApp.prototype.Constructor = function(self, theArguments) {
             _myMovieNode.decoderhint = theDecoderHint;
             _myMovieNode.audio_stream = 0;
             //_myMovieNode.maxcachesize = 32;
-            _myMovieNode.targetpixelformat = "RGB";//"YUV420";//"ALPHA";//"LUMINANCE8";//"RGB" // "ALPHA";//"RGBA8";
+            _myMovieNode.targetpixelformat = "RGB";//"YUVA420";//"YUV420";//"ALPHA";//"LUMINANCE8";//"RGB" // "ALPHA";//"RGBA8";
         }
 
         if (_myFullSizeMode) {
