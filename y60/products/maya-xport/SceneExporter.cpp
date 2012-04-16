57462cf8428fea88c3ba9c7740df0f475b42d403
gunnar <gunnar@artcom.de>
2012-04-16 19:04:25 +0200

    (maya-xport) enable build with maya > 2009

--- y60/products/maya-xport/SceneExporter.cpp
+++ y60/products/maya-xport/SceneExporter.cpp
@@ -111,7 +111,7 @@ using namespace asl;
 using namespace y60;
 using namespace dom;
 
-#if MAYA_API_VERSION < 700 || MAYA_API_VERSION > 200900
+#if MAYA_API_VERSION < 700
     #error "Y60 Maya exporter not yet released for this Maya version"
 #endif
 #ifdef LINUX
