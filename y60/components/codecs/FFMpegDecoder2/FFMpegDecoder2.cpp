0e013177128836e4b3abc6197d571e163eb072b2
gunnar <gunnar.marten@artcom.de>
2012-07-06 17:55:09 +0100

    (y60/FFMpegDecoder2) minor beautification

--- y60/components/codecs/FFMpegDecoder2/FFMpegDecoder2.cpp
+++ y60/components/codecs/FFMpegDecoder2/FFMpegDecoder2.cpp
@@ -603,8 +603,7 @@ namespace y60 {
                     //XXX: no thread lock
                     if (_myVideoStartTimestamp == -1) {
                         _myVideoStartTimestamp = myPacket->dts;
-                        double myTime = _myVideoStartTimestamp / (1/av_q2d(_myVStream->time_base));
-                        _myAudioTimeOffset = myTime;
+                        _myAudioTimeOffset = _myVideoStartTimestamp / (1/av_q2d(_myVStream->time_base));
                         DBV(AC_DEBUG << "---- setting video start timestamp: "<<_myVideoStartTimestamp
                                      << " setting audio offset to: " << _myAudioTimeOffset;)
                     }
