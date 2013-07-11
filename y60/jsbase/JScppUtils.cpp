1b73a738192bd4387a1745337dd405e32bae0bea
Martin Skinner <martin@artcom.de>
2013-07-11 09:40:37 +0200

    changed isValidUTF8 to use boost utf8 functions.

--- y60/jsbase/JScppUtils.cpp
+++ y60/jsbase/JScppUtils.cpp
@@ -66,7 +66,7 @@
 
 using namespace std;
 using namespace asl;
-using namespace boost::locale::conv;
+using namespace boost::locale;
 
 #define DB(x) // x;
 
@@ -103,8 +103,8 @@ as_string(JSContext *cx, jsval theVal) {
 
     try {
         // now convert to utf-8 encoded string
-        myResult = utf_to_utf<char>(myData, myData+srcLen, stop);
-    } catch (boost::locale::conv::conversion_error & ex) {
+        myResult = conv::utf_to_utf<char>(myData, myData+srcLen, conv::stop);
+    } catch (conv::conversion_error & ex) {
         throw jslib::UnicodeException(ex.what(), PLUS_FILE_LINE);
     }
     return myResult;
@@ -183,15 +183,19 @@ jsval as_jsval(JSContext *cx, int theValue) {
 }
 
 bool
-isValidUTF8(const char * theU8String) {
+isValidUTF8(const std::string & theU8String) {
 #ifdef _WIN32
     AC_SIZE_TYPE myWCharSize = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, theU8String, -1, 0, 0);
     return myWCharSize != 0;
 #else
-    GError *error = NULL;
-    gunichar2 * myUTF16 = g_utf8_to_utf16(theU8String, -1,0,0,&error);
-    return myUTF16;
-
+    std::string::const_iterator it = theU8String.begin();
+    while (it != theU8String.end()) {
+        utf::code_point cp = utf::utf_traits<char>::decode(it, theU8String.end());
+        if (!utf::is_valid_codepoint(cp)) {
+            break;
+        }
+    }
+    return it == theU8String.end();
 #endif
 };
 
@@ -217,9 +221,9 @@ as_jsval(JSContext *cx, const char * theU8String) {
 #else
     JSString * myString;
     try {
-        std::basic_string<asl::Unsigned16> utf16String = utf_to_utf<asl::Unsigned16>(theU8String, stop);
+        std::basic_string<asl::Unsigned16> utf16String = conv::utf_to_utf<asl::Unsigned16>(theU8String, conv::stop);
         myString = JS_NewUCStringCopyN(cx,utf16String.c_str(), utf16String.size());
-    } catch (boost::locale::conv::conversion_error & ex) {
+    } catch (conv::conversion_error & ex) {
         ostringstream os;
         os << ex.what() << " '" << theU8String << "' hex:";
         for (unsigned i = 0; i < strlen(theU8String); ++i) {
@@ -234,9 +238,9 @@ as_jsval(JSContext *cx, const char * theU8String) {
 jsval as_jsval(JSContext *cx, const std::string & theValue) {
     JSString * myString;
     try {
-        std::basic_string<asl::Unsigned16> utf16String = utf_to_utf<asl::Unsigned16>(theValue, stop);
+        std::basic_string<asl::Unsigned16> utf16String = conv::utf_to_utf<asl::Unsigned16>(theValue, conv::stop);
         myString = JS_NewUCStringCopyN(cx,utf16String.c_str(), utf16String.size());
-    } catch (boost::locale::conv::conversion_error & ex) {
+    } catch (conv::conversion_error & ex) {
         ostringstream os;
         os << ex.what() << " '" << theValue << "' hex:";
         for (unsigned i = 0; i < theValue.size(); ++i) {
--- y60/jsbase/JScppUtils.h
+++ y60/jsbase/JScppUtils.h
@@ -372,7 +372,7 @@ Y60_JSBASE_DECL std::string
 searchFileRelativeToJSInclude(JSContext *cx, JSObject *obj, uintN argc, jsval *argv,
                               const std::string & theFile);
 
-bool isValidUTF8(const char * theU8String);
+bool isValidUTF8(const std::string & theU8String);
 
 }
 
